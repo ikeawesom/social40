e8dfb337c95351f289566ae1bc494e85af5e575a
ikeawesom <ikelimjl@gmail.com>
2023-10-19 14:25:01 +0800

    firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC9vJ-3Gva493SG4ixrQRJUg9HwnFYG1U0",
+  authDomain: "social40-15b92.firebaseapp.com",
+  projectId: "social40-15b92",
+  storageBucket: "social40-15b92.appspot.com",
+  messagingSenderId: "226245662",
+  appId: "1:226245662:web:76bfcd0012a5ae87bd420f",
+  measurementId: "G-EZHDZC44JN",
+};
+
+// Initialize Firebase
+export const FIREBASE_APP = initializeApp(firebaseConfig);
