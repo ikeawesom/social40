86b277aeeee400a0cf98ae62a24b115c9ae2a179
ikeawesom <ikelimjl@gmail.com>
2023-10-19 16:45:35 +0800

    added signout button

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import SignoutButton from "@/src/components/utils/SignoutButton";
+import { getCookies } from "next-client-cookies/server";
+
 export default function Home() {
-  return <></>;
+  const cookieStore = getCookies();
+  const UID = cookieStore.get("UID");
+
+  if (UID)
+    return (
+      <>
+        <SignoutButton />
+      </>
+    );
 }
