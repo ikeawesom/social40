89137d1fbec211113d0ac821a67d86d23c87e508
ikeawesom <ikelimjl@gmail.com>
2023-10-20 12:01:16 +0800

    used new auth method

--- src/components/utils/SignoutButton.tsx
+++ src/components/utils/SignoutButton.tsx
@@ -3,10 +3,13 @@ import React from "react";
 import PrimaryButton from "./PrimaryButton";
 import { authHandler } from "@/src/firebase/auth";
 import { toast } from "sonner";
+import { getAuth } from "firebase/auth";
+import { FIREBASE_APP } from "@/src/firebase/config";
 
 export default function SignoutButton() {
   const handleSignout = async () => {
-    const res = await authHandler.signOutUser();
+    const auth = getAuth(FIREBASE_APP);
+    const res = await authHandler.signOutUser(auth);
     if (!res.status) toast.error(res.data);
   };
   return <PrimaryButton onClick={handleSignout}>Sign out</PrimaryButton>;
