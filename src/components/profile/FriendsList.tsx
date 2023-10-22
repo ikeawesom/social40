4cf84d57e49cd6a926d21c59045c2b90ec8ef0d4
ikeawesom <ikelimjl@gmail.com>
2023-10-23 02:19:59 +0800

    friends section matching schema

--- src/components/profile/FriendsList.tsx
+++ src/components/profile/FriendsList.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { DataType } from "@/app/(navigation)/profile/page";
 import Image from "next/image";
+import { FriendsListType } from "./ProfileSection";
 
-export default function FriendsList({ friendsList }: DataType) {
-  if (friendsList)
+export default function FriendsList({
+  friendsData,
+  className,
+}: FriendsListType) {
+  if (friendsData) {
+    const empty = Object.keys(friendsData).length === 0;
+
     return (
-      <div className="flex flex-col items-start justify-start gap-y-1">
+      <div className="flex flex-col items-start justify-start gap-y-1 w-full">
         <h1 className="text-start font-semibold text-base">Friends</h1>
-        {friendsList.length !== 0 ? (
+        {!empty ? (
           <ul className="flex flex-col items-start justify-start">
-            {friendsList.map((item, index) => (
+            {Object.keys(friendsData).map((memberID: string) => (
               <li
-                className="flex flex-row gap-x-1 items-center justify-start"
-                key={index}
+                className="flex flex-row gap-x-3 items-center justify-start"
+                key={memberID}
               >
                 <Image
                   src="icons/icon_avatar.svg"
@@ -23,10 +28,10 @@ export default function FriendsList({ friendsList }: DataType) {
                 />
                 <div className="flex flex-col items-start justify-start">
                   <h1 className="font-bold text-custom-dark-text text-base">
-                    {item.displayName}
+                    {friendsData[memberID].displayName}
                   </h1>
                   <p className="text-center text-custom-grey-text text-xs">
-                    {item.username}
+                    {friendsData[memberID].memberID}
                   </p>
                 </div>
               </li>
@@ -39,4 +44,5 @@ export default function FriendsList({ friendsList }: DataType) {
         )}
       </div>
     );
+  }
 }
--- src/components/profile/ProfileSection.tsx
+++ src/components/profile/ProfileSection.tsx
@@ -3,46 +3,55 @@ import DefaultCard from "../DefaultCard";
 import Image from "next/image";
 import SecondaryButton from "../utils/SecondaryButton";
 import { twMerge } from "tailwind-merge";
-import { DataType } from "@/app/(navigation)/profile/page";
 import PrimaryButton from "../utils/PrimaryButton";
 import HRow from "../utils/HRow";
 import FriendsList from "./FriendsList";
 import SignoutButton from "../utils/SignoutButton";
+import { FRIENDS_SCHEMA, MEMBER_SCHEMA } from "@/src/utils/schemas/members";
+import { getCookies } from "next-client-cookies/server";
+import fetchUserDataServer from "@/src/utils/fetchUserDataServer";
+
+export type FriendsListType = {
+  friendsData: { [key: string]: MEMBER_SCHEMA };
+  className?: string;
+};
 
 export default function ProfileSection({
-  data,
+  friendsData,
   className,
-  friendsList,
-}: DataType) {
-  return (
-    <DefaultCard
-      className={twMerge(
-        "flex flex-col gap-y-3 items-center justify-center relative",
-        className
-      )}
-    >
-      <div className="flex flex-col gap-2 items-center justify-center">
-        <SignoutButton />
-        <Image
-          src="icons/icon_avatar.svg"
-          height={80}
-          width={80}
-          alt="Profile"
-          className="drop-shadow-md"
-        />
-        <div className="flex flex-col items-center justify-center gap-0">
-          <h1 className="font-bold text-custom-dark-text text-base">
-            {data.displayName}
-          </h1>
-          <p className="text-center text-custom-grey-text text-xs">
-            {data.username}
-          </p>
+}: FriendsListType) {
+  const memberDetails = fetchUserDataServer();
+
+  if (memberDetails)
+    return (
+      <DefaultCard
+        className={twMerge(
+          "flex flex-col gap-y-3 items-center justify-center relative",
+          className
+        )}
+      >
+        <div className="flex flex-col gap-2 items-center justify-center">
+          <SignoutButton />
+          <Image
+            src="icons/icon_avatar.svg"
+            height={80}
+            width={80}
+            alt="Profile"
+            className="drop-shadow-md"
+          />
+          <div className="flex flex-col items-center justify-center gap-0">
+            <h1 className="font-bold text-custom-dark-text text-base">
+              {memberDetails.displayName}
+            </h1>
+            <p className="text-center text-custom-grey-text text-xs">
+              {memberDetails.memberID}
+            </p>
+          </div>
         </div>
-      </div>
-      <SecondaryButton>Edit Profile</SecondaryButton>
-      <PrimaryButton>Invite Friends</PrimaryButton>
-      <HRow />
-      <FriendsList data={data} friendsList={friendsList} />
-    </DefaultCard>
-  );
+        <SecondaryButton>Edit Profile</SecondaryButton>
+        <PrimaryButton>Invite Friends</PrimaryButton>
+        <HRow />
+        <FriendsList friendsData={friendsData} />
+      </DefaultCard>
+    );
 }
