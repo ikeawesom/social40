6a4f1e023ffc39f8e03f892c3d5f9a4dd8c019d7
ikeawesom <ikelimjl@gmail.com>
2023-10-23 23:36:56 +0800

    error handling

--- src/components/profile/FriendsList.tsx
+++ src/components/profile/FriendsList.tsx
@@ -1,58 +1,59 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { FriendsListType } from "./ProfileSection";
-import { getFriendsList } from "@/src/utils/profile/getFriendsList";
 import { useAuth } from "@/src/contexts/AuthContext";
+import { useFriendsList } from "@/src/hooks/profile/useFriendsList";
+import { toast } from "sonner";
+import LoadingIcon from "../utils/LoadingIcon";
 
 export default function FriendsList() {
-  const [friendsData, setFriendData] = useState<FriendsListType>();
   const { memberID } = useAuth();
 
-  useEffect(() => {
-    const handleFetch = async (memberID: string) => {
-      const res = await getFriendsList({ memberID });
-      setFriendData(res);
-    };
-    if (memberID) handleFetch(memberID);
-  }, [friendsData, memberID]);
+  if (memberID) {
+    const { friendsData } = useFriendsList(memberID);
 
-  if (friendsData) {
-    const empty = Object.keys(friendsData).length === 0;
+    if (friendsData) {
+      const empty = Object.keys(friendsData).length === 0;
 
-    return (
-      <div className="flex flex-col items-start justify-start gap-y-1 w-full">
-        <h1 className="text-start font-semibold text-base">Friends</h1>
-        {!empty ? (
-          <ul className="flex flex-col items-start justify-start">
-            {Object.keys(friendsData).map((memberID: string) => (
-              <li
-                className="flex flex-row gap-x-3 items-center justify-start"
-                key={memberID}
-              >
-                <Image
-                  src="icons/icon_avatar.svg"
-                  height={40}
-                  width={40}
-                  alt="Profile"
-                  className="drop-shadow-md"
-                />
-                <div className="flex flex-col items-start justify-start">
-                  <h1 className="font-bold text-custom-dark-text text-base">
-                    {friendsData[memberID].displayName}
-                  </h1>
-                  <p className="text-center text-custom-grey-text text-xs">
-                    {friendsData[memberID].memberID}
-                  </p>
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p className="text-start text-custom-grey-text text-xs">
-            No friends added. Invite someone today!
-          </p>
-        )}
-      </div>
-    );
+      return (
+        <div className="flex flex-col items-start justify-start gap-y-1 w-full">
+          <h1 className="text-start font-semibold text-base">Friends</h1>
+          {!empty ? (
+            <ul className="flex flex-col items-start justify-start">
+              {Object.keys(friendsData).map((memberID: string) => (
+                <li
+                  className="flex flex-row gap-x-3 items-center justify-start"
+                  key={memberID}
+                >
+                  <Image
+                    src="icons/icon_avatar.svg"
+                    height={40}
+                    width={40}
+                    alt="Profile"
+                    className="drop-shadow-md"
+                  />
+                  <div className="flex flex-col items-start justify-start">
+                    <h1 className="font-bold text-custom-dark-text text-base">
+                      {friendsData[memberID].displayName}
+                    </h1>
+                    <p className="text-center text-custom-grey-text text-xs">
+                      {friendsData[memberID].memberID}
+                    </p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-start text-custom-grey-text text-xs">
+              No friends added. Invite someone today!
+            </p>
+          )}
+        </div>
+      );
+    } else if (friendsData === null) {
+      toast.error(
+        "An error has occurred while loading your friends list. Please refresh your page to try again."
+      );
+    }
   }
+  return <LoadingIcon />;
 }
