18f630939b80836a4c05f2d3ea4df48d3f1e952e
ikeawesom <ikelimjl@gmail.com>
2023-10-24 00:33:00 +0800

    refactored groups page

--- app/(navigation)/groups/page.tsx
+++ app/(navigation)/groups/page.tsx
@@ -1,34 +1,12 @@
-import LoadingScreen from "@/src/components/screens/LoadingScreen";
-import GroupsCreatedSection from "@/src/components/groups/GroupsCreatedSection";
-import GroupsJoinedSection from "@/src/components/groups/GroupsJoinedSection";
-import HeaderBar from "@/src/components/navigation/HeaderBar";
-import { getJoinedGroups } from "@/src/utils/groups/getJoinedGroups";
-import { getOwnedGroups } from "@/src/utils/groups/getOwnedGroups";
-import useFetchUserDataServer from "@/src/utils/useFetchUserDataServer";
 import React from "react";
+import HeaderBar from "@/src/components/navigation/HeaderBar";
+import GroupContainer from "@/src/components/groups/GroupContainer";
 
 export default async function Groups() {
-  const data = useFetchUserDataServer();
-
-  if (data) {
-    const memberID = data.memberID;
-    const role = data.role;
-
-    const [groupsCreated, groupsJoined] = await Promise.all([
-      getOwnedGroups({ memberID }),
-      getJoinedGroups({ memberID }),
-    ]);
-    return (
-      <>
-        <HeaderBar text="Groups" />
-        <div className="flex flex-col gap-10 items-center justify-start w-full">
-          {role !== "member" && (
-            <GroupsCreatedSection ownedGroups={groupsCreated} />
-          )}
-          <GroupsJoinedSection joinedGroups={groupsJoined} />
-        </div>
-      </>
-    );
-  }
-  return <LoadingScreen />;
+  return (
+    <>
+      <HeaderBar text="Groups" />
+      <GroupContainer />
+    </>
+  );
 }
