f15e3cce4851d40002ab68aee974a2d3d59536d1
ikeawesom <ikelimjl@gmail.com>
2023-10-24 01:46:03 +0800

    increased top spacing

--- app/(navigation)/layout.tsx
+++ app/(navigation)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/src/components/navigation/Navbar";
 export default function NavLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="mb-16 mt-10">{children}</div>
+      <div className="mb-16 mt-14">{children}</div>
       <Navbar />
     </>
   );
