02c85b19b55bd87475440fdece4d0fc7fe07dab3
ikeawesom <ikelimjl@gmail.com>
2023-10-24 00:03:31 +0800

    using local storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -30,6 +30,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         })}`;
 
         setMember(null);
+        localStorage.clear();
         router.push(route, { scroll: false });
       }
     });
