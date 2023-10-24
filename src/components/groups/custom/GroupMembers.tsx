118b08aa72f4cadc7358dfa572ecc034085a723e
ikeawesom <ikelimjl@gmail.com>
2023-10-24 19:21:59 +0800

    container to display members

---
+++ src/components/groups/custom/GroupMembers.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useGroupMembers } from "@/src/hooks/groups/custom/useGroupMembers";
+import LoadingIcon from "../../utils/LoadingIcon";
+import DefaultCard from "../../DefaultCard";
+import HRow from "../../utils/HRow";
+import InnerContainer from "../../utils/InnerContainer";
+import GroupMemberTab from "./GroupMemberTab";
+import { toast } from "sonner";
+
+export default function GroupMembers({ groupID }: { groupID: string }) {
+  const { membersList, error } = useGroupMembers(groupID);
+
+  if (membersList)
+    return (
+      <DefaultCard className="w-full flex flex-col items-start justify-start gap-2 max-h-[80vh]">
+        <div className="w-full">
+          <h1 className="text-custom-dark-text font-semibold flex gap-1 items-center justify-start text-start">
+            Members
+          </h1>
+          <HRow />
+        </div>
+        <InnerContainer>
+          {Object.keys(membersList).map((item) => (
+            <GroupMemberTab key={item} data={membersList[item]} />
+          ))}
+        </InnerContainer>
+      </DefaultCard>
+    );
+  if (error) {
+    toast.error(error);
+  }
+  return <LoadingIcon />;
+}
