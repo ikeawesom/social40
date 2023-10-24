9f327999a47bf6ba38374ac1875067e83087249e
ikeawesom <ikelimjl@gmail.com>
2023-10-24 12:30:33 +0800

    waitlist section with load states

--- src/components/groups/custom/GroupRequested.tsx
+++ src/components/groups/custom/GroupRequested.tsx
@@ -1,8 +1,20 @@
-"use client";
-import { useMemberID } from "@/src/hooks/useMemberID";
 import React from "react";
+import LoadingScreenSmall from "../../screens/LoadingScreenSmall";
+import RequestsSection from "./requests/RequestsSection";
+import { useGroupRequests } from "@/src/hooks/groups/custom/requests/useGroupRequests";
 
-export default function GroupRequested() {
-  const { memberID } = useMemberID();
-  return <div>GroupRequested</div>;
+export default function GroupRequested({ groupID }: { groupID: string }) {
+  const { requested } = useGroupRequests(groupID);
+
+  return (
+    <div className="w-full">
+      {requested === undefined ? (
+        <LoadingScreenSmall width={30} height={30} />
+      ) : requested === null ? (
+        <RequestsSection empty />
+      ) : (
+        <RequestsSection data={requested} />
+      )}
+    </div>
+  );
 }
