bf528b9e1671af5ba06bd2f01b695468ca4a6746
ikeawesom <ikelimjl@gmail.com>
2023-10-24 10:57:49 +0800

    reduced font weight

--- src/components/groups/GroupItem.tsx
+++ src/components/groups/GroupItem.tsx
@@ -14,7 +14,7 @@ export default function GroupItem({ title, subtitle }: GroupItemType) {
       className="flex flex-col items-start justify-center w-full bg-white rounded-lg py-2 px-3 shadow-sm hover:brightness-95 duration-300"
       key={title}
     >
-      <h1 className="font-semibold text-base">{title}</h1>
+      <h1 className="font-medium text-base">{title}</h1>
       <p className="text-xs text-custom-grey-text">{subtitle}</p>
     </div>
   );
