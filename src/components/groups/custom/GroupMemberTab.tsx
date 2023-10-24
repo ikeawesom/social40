4a43e277067af5c1a8302e4c5804fc948c696e97
ikeawesom <ikelimjl@gmail.com>
2023-10-24 22:10:02 +0800

    tab styles

--- src/components/groups/custom/GroupMemberTab.tsx
+++ src/components/groups/custom/GroupMemberTab.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
 import { MAX_LENGTH } from "./requests/RequestedUser";
+import { useMemberID } from "@/src/hooks/useMemberID";
 
 export default function GroupMemberTab({
   data,
@@ -15,8 +16,9 @@ export default function GroupMemberTab({
   };
   className?: string;
 }) {
+  const { memberID } = useMemberID();
   const displayName = data.displayName;
-  const memberID = data.memberID;
+  const groupMemberID = data.memberID;
   const bookedIn = data.bookedIn;
   const dateJoined = data.dateJoined;
   const role = data.role;
@@ -24,28 +26,31 @@ export default function GroupMemberTab({
   return (
     <div
       className={twMerge(
-        "w-full py-2 px-3 shadow-sm hover:bg-custom-light-text duration-300 flex items-center justify-between",
+        "w-full py-2 px-3 shadow-sm duration-300 flex items-center justify-between",
+        memberID === groupMemberID
+          ? "bg-custom-light-orange hover:brightness-75"
+          : "hover:bg-custom-light-text",
         className
       )}
     >
       <div className="flex flex-col items-start justify-center">
         <div className="flex items-center justify-start gap-1">
-          <span
-            className={`h-2 w-2 rounded-full ${
-              bookedIn ? "bg-custom-green" : "bg-custom-orange"
-            }`}
-          />
           <h1 className="font-bold text-sm text-custom-dark-text">
             {displayName}
           </h1>
           {role === "owner" && (
             <p className="text-xs text-custom-green">(owner)</p>
           )}
+          <span
+            className={`h-2 w-2 rounded-full ${
+              bookedIn ? "bg-custom-green" : "bg-custom-orange"
+            }`}
+          />
         </div>
         <p className="text-xs text-custom-grey-text">
-          {memberID.length > MAX_LENGTH
-            ? memberID.substring(0, MAX_LENGTH - 3) + "..."
-            : memberID}
+          {groupMemberID.length > MAX_LENGTH
+            ? groupMemberID.substring(0, MAX_LENGTH - 3) + "..."
+            : groupMemberID}
         </p>
         <p className="text-xs text-custom-grey-text">Joined on: {dateJoined}</p>
       </div>
