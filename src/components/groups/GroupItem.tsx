5ef04bf70053183aad688f7b8506d6ae09d1ea21
ikeawesom <ikelimjl@gmail.com>
2023-10-24 12:10:35 +0800

    added customization

--- src/components/groups/GroupItem.tsx
+++ src/components/groups/GroupItem.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
 
 type GroupItemType = {
   title: string;
   subtitle: string;
+  className?: string;
 };
 
-export default function GroupItem({ title, subtitle }: GroupItemType) {
+export default function GroupItem({
+  title,
+  subtitle,
+  className,
+}: GroupItemType) {
   const router = useRouter();
   return (
     <div
       onClick={() => router.push(`/groups/${title}`, { scroll: false })}
-      className="flex flex-col items-start justify-center w-full bg-white rounded-lg py-2 px-3 shadow-sm hover:brightness-95 duration-300"
+      className={twMerge(
+        "flex flex-col items-start justify-center w-full bg-white rounded-lg py-2 px-3 shadow-sm hover:brightness-95 duration-300",
+        className
+      )}
       key={title}
     >
       <h1 className="font-medium text-base">{title}</h1>
