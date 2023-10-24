5ed0672784e1f42a170ff3f9a0c0d0971b6458ac
ikeawesom <ikelimjl@gmail.com>
2023-10-24 12:11:07 +0800

    added customization

--- src/components/screens/LoadingScreenSmall.tsx
+++ src/components/screens/LoadingScreenSmall.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import LoadingIcon from "../utils/LoadingIcon";
 
-export default function LoadingScreenSmall() {
+export default function LoadingScreenSmall({
+  width,
+  height,
+}: {
+  width?: number;
+  height?: number;
+}) {
   return (
     <div className="grid place-items-center w-full h-[40vh]">
-      <LoadingIcon height={80} width={80} />
+      <LoadingIcon height={height ? height : 80} width={width ? width : 80} />
     </div>
   );
 }
