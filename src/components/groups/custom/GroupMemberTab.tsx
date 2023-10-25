56c954e20cd7b47885f3b0197188baa1bdb37faa
ikeawesom <ikelimjl@gmail.com>
2023-10-25 13:27:35 +0800

    added criterias

--- src/components/groups/custom/GroupMemberTab.tsx
+++ src/components/groups/custom/GroupMemberTab.tsx
@@ -3,6 +3,7 @@ import { twMerge } from "tailwind-merge";
 import { MAX_LENGTH } from "./requests/RequestedUser";
 import { useMemberID } from "@/src/hooks/useMemberID";
 import { useRouter } from "next/navigation";
+import StatusDot from "../../utils/StatusDot";
 
 export default function GroupMemberTab({
   data,
@@ -19,44 +20,43 @@ export default function GroupMemberTab({
 }) {
   const { memberID } = useMemberID();
   const router = useRouter();
-  const displayName = data.displayName;
+  const displayName = data.displayName as string;
   const groupMemberID = data.memberID;
-  const bookedIn = data.bookedIn;
+  const bookedIn = data.bookedIn as boolean;
   const dateJoined = data.dateJoined;
   const role = data.role;
 
-  return (
-    <div
-      onClick={() => router.push(`/members/${groupMemberID}`)}
-      className={twMerge(
-        "w-full py-2 px-3 shadow-sm duration-300 flex items-center justify-between cursor-pointer",
-        memberID === groupMemberID
-          ? "bg-custom-light-orange hover:brightness-95"
-          : "hover:bg-custom-light-text",
-        className
-      )}
-    >
-      <div className="flex flex-col items-start justify-center">
-        <div className="flex items-center justify-start gap-1">
-          <h1 className="font-bold text-sm text-custom-dark-text">
-            {displayName}
-          </h1>
-          {role === "owner" && (
-            <p className="text-xs text-custom-green">(owner)</p>
-          )}
-          <span
-            className={`h-2 w-2 rounded-full ${
-              bookedIn ? "bg-custom-green" : "bg-custom-orange"
-            }`}
-          />
+  if (data)
+    return (
+      <div
+        onClick={() => router.push(`/members/${groupMemberID}`)}
+        className={twMerge(
+          "w-full py-2 px-3 shadow-sm duration-300 flex items-center justify-between cursor-pointer",
+          memberID === groupMemberID
+            ? "bg-custom-light-orange hover:brightness-95"
+            : "hover:bg-custom-light-text",
+          className
+        )}
+      >
+        <div className="flex flex-col items-start justify-center">
+          <div className="flex items-center justify-start gap-1">
+            <h1 className="font-bold text-sm text-custom-dark-text">
+              {displayName}
+            </h1>
+            {role === "owner" && (
+              <p className="text-xs text-custom-green">(owner)</p>
+            )}
+            <StatusDot status={bookedIn} />
+          </div>
+          <p className="text-xs text-custom-grey-text">
+            {groupMemberID.length > MAX_LENGTH
+              ? groupMemberID.substring(0, MAX_LENGTH - 3) + "..."
+              : groupMemberID}
+          </p>
+          <p className="text-xs text-custom-grey-text">
+            Joined on: {dateJoined}
+          </p>
         </div>
-        <p className="text-xs text-custom-grey-text">
-          {groupMemberID.length > MAX_LENGTH
-            ? groupMemberID.substring(0, MAX_LENGTH - 3) + "..."
-            : groupMemberID}
-        </p>
-        <p className="text-xs text-custom-grey-text">Joined on: {dateJoined}</p>
       </div>
-    </div>
-  );
+    );
 }
