b7b71c4990149a1bdf12c35e7197e09e6a83781e
ikeawesom <ikelimjl@gmail.com>
2023-10-25 17:13:47 +0800

    solo image

--- src/components/utils/LoadingIcon.tsx
+++ src/components/utils/LoadingIcon.tsx
@@ -8,13 +8,12 @@ type LoadingType = {
 
 export default function LoadingIcon({ height, width }: LoadingType) {
   return (
-    <div className="animate-spin">
-      <Image
-        src="/icons/icon_spinner.svg"
-        width={height ? height : 100}
-        height={width ? width : 100}
-        alt="Loading..."
-      />
-    </div>
+    <Image
+      src="/icons/icon_spinner.svg"
+      width={height ? height : 100}
+      height={width ? width : 100}
+      alt="Loading..."
+      className="animate-spin"
+    />
   );
 }
