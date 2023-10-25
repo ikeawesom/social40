24b1f3ee063da12622464ad748c9f12c8cfd5ca4
ikeawesom <ikelimjl@gmail.com>
2023-10-25 13:37:29 +0800

    added customizatoin

--- src/components/utils/StatusDot.tsx
+++ src/components/utils/StatusDot.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
-export default function StatusDot({ status }: { status: boolean }) {
+export default function StatusDot({
+  status,
+  className,
+}: {
+  status: boolean;
+  className?: string;
+}) {
   return (
     <span
-      className={`h-2 w-2 rounded-full ${
-        status ? "bg-custom-green" : "bg-custom-orange"
-      }`}
+      className={twMerge(
+        `h-2 w-2 rounded-full ${
+          status ? "bg-custom-green" : "bg-custom-orange"
+        }`,
+        className
+      )}
     />
   );
 }
