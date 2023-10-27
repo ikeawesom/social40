e9cfbf6519a0800f1ce78b9aea2aee03b5f6ae2e
ikeawesom <ikelimjl@gmail.com>
2023-10-27 11:43:02 +0800

    added criteria for booking people in

--- src/components/bibo/BiboSection.tsx
+++ src/components/bibo/BiboSection.tsx
@@ -5,6 +5,7 @@ import HRow from "../utils/HRow";
 import { useProfile } from "@/src/hooks/profile/useProfile";
 import PrimaryButton from "../utils/PrimaryButton";
 import { useRouter } from "next/navigation";
+import { ROLES_HIERARCHY } from "../members/MemberProfileContainer";
 
 export default function BiboSection() {
   const router = useRouter();
@@ -16,25 +17,30 @@ export default function BiboSection() {
       empty = false;
     }
 
-    return (
-      <DefaultCard>
-        <div className="flex flex-col items-start justify-start gap-y-1 w-full">
-          <h1 className="text-start font-semibold text-base">Booked In</h1>
-          {!empty ? (
-            <></>
-          ) : (
-            <p className="text-start text-custom-grey-text text-xs">
-              You have not booked in anyone before.
-            </p>
-          )}
-        </div>
-        <PrimaryButton
-          onClick={() => router.push("/book-someone")}
-          className="mt-2"
-        >
-          Book Someone In
-        </PrimaryButton>
-      </DefaultCard>
-    );
+    if (memberDetails) {
+      const role = memberDetails.role;
+      const aboveAdmin = ROLES_HIERARCHY[role] >= ROLES_HIERARCHY["admin"];
+      if (aboveAdmin)
+        return (
+          <DefaultCard>
+            <div className="flex flex-col items-start justify-start gap-y-1 w-full">
+              <h1 className="text-start font-semibold text-base">Booked In</h1>
+              {!empty ? (
+                <></>
+              ) : (
+                <p className="text-start text-custom-grey-text text-xs">
+                  You have not booked in anyone before.
+                </p>
+              )}
+            </div>
+            <PrimaryButton
+              onClick={() => router.push("/book-someone")}
+              className="mt-2"
+            >
+              Book Someone In
+            </PrimaryButton>
+          </DefaultCard>
+        );
+    }
   }
 }
