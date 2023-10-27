f534c7f58b2fd14199faabd5b42d2f10126307cf
ikeawesom <ikelimjl@gmail.com>
2023-10-27 20:24:29 +0800

    cleaned up

--- app/(navigation)/groups/[groupID]/page.tsx
+++ app/(navigation)/groups/[groupID]/page.tsx
@@ -1,13 +1,7 @@
-import CustomGroupContainer from "@/src/components/groups/custom/CustomGroupContainer";
-import GroupHeader from "@/src/components/groups/custom/GroupHeader";
-import GroupRequested from "@/src/components/groups/custom/GroupRequested";
-import HeaderBar from "@/src/components/navigation/HeaderBar";
-import NotFoundScreen from "@/src/components/screens/NotFoundScreen";
-import OfflineScreen from "@/src/components/screens/OfflineScreen";
-import { dbHandler } from "@/src/firebase/db";
-import { GROUP_SCHEMA } from "@/src/utils/schemas/groups";
-import { Metadata } from "next";
 import React from "react";
+import { Metadata } from "next";
+import CustomGroupContainer from "@/src/components/groups/custom/CustomGroupContainer";
+import HeaderBar from "@/src/components/navigation/HeaderBar";
 
 export const metadata: Metadata = {
   title: "Groups",
