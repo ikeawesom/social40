f7f85949b0a418d2c51bf498133bb8784d1f75fe
Ike <ikelimjl@gmail.com>
2023-10-28 21:33:32 +0800

    group settings section

---
+++ src/components/groups/custom/settings/SettingsSection.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import DefaultCard from "@/src/components/DefaultCard";
+import PrimaryButton from "@/src/components/utils/PrimaryButton";
+import { useRouter } from "next/navigation";
+
+export default function SettingsSection({ groupID }: { groupID: string }) {
+  const router = useRouter();
+  return (
+    <DefaultCard className="w-full flex flex-col items-center justify-center gap-2">
+      <p className="text-sm text-custom-primary text-center">
+        Admin ID: {groupID}
+      </p>
+      <PrimaryButton
+        onClick={() => {
+          router.push(`/groups/${groupID}/settings`);
+        }}
+      >
+        Group Settings
+      </PrimaryButton>
+    </DefaultCard>
+  );
+}
