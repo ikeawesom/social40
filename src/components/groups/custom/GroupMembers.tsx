9eba917ab5b3e2451d813029238f0d1213d6162b
Ike <ikelimjl@gmail.com>
2023-10-28 21:04:32 +0800

    removed empty criteria

--- src/components/groups/custom/GroupMembers.tsx
+++ src/components/groups/custom/GroupMembers.tsx
@@ -11,7 +11,6 @@ export default function GroupMembers({ groupID }: { groupID: string }) {
   const { membersList, error } = useGroupMembers(groupID);
 
   if (membersList) {
-    const empty = Object.keys(membersList).length === 0;
     return (
       <DefaultCard className="w-full flex flex-col items-start justify-start gap-2 max-h-[40vh]">
         <div className="w-full">
@@ -21,17 +20,9 @@ export default function GroupMembers({ groupID }: { groupID: string }) {
           <HRow />
         </div>
         <InnerContainer>
-          {!empty ? (
-            Object.keys(membersList).map((item) => (
-              <GroupMemberTab key={item} data={membersList[item]} />
-            ))
-          ) : (
-            <>
-              <p className="text-start text-custom-grey-text text-xs">
-                No members yet. Invite others and start tracking!
-              </p>
-            </>
-          )}
+          {Object.keys(membersList).map((item) => (
+            <GroupMemberTab key={item} data={membersList[item]} />
+          ))}
         </InnerContainer>
       </DefaultCard>
     );
