e4e1727e0aea1558768d53a9966c1504d6837c88
Ike <ikelimjl@gmail.com>
2023-10-29 12:27:33 +0800

    fetched memberID from API

--- src/hooks/useMemberID.ts
+++ src/hooks/useMemberID.ts
@@ -1,11 +1,19 @@
+import { cookies } from "next/headers";
 import { useEffect, useState } from "react";
+import { useHostname } from "./useHostname";
 
 export function useMemberID() {
   const [memberID, setMemberID] = useState("");
+  const { host } = useHostname();
 
   useEffect(() => {
-    const stored = localStorage.getItem("memberID");
-    if (stored) setMemberID(stored);
+    const getMemberID = async () => {
+      const res = await fetch(`${host}/api/auth/signin`);
+      const fetchedData = await res.json();
+      const { data } = fetchedData;
+      setMemberID(data);
+    };
+    if (memberID === "") getMemberID();
   }, [memberID]);
 
   return { memberID, setMemberID };
