acf21814f02e2032ef21cf229696ca5532ad3197
ikeawesom <ikelimjl@gmail.com>
2023-10-30 15:16:35 +0800

    dynamic page title

--- app/(navigation)/groups/[groupID]/page.tsx
+++ app/(navigation)/groups/[groupID]/page.tsx
@@ -3,9 +3,16 @@ import { Metadata } from "next";
 import CustomGroupContainer from "@/src/components/groups/custom/CustomGroupContainer";
 import HeaderBar from "@/src/components/navigation/HeaderBar";
 
-export const metadata: Metadata = {
-  title: "Groups",
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: { [groupID: string]: string };
+}) {
+  const groupID = params.groupID;
+  return {
+    title: groupID,
+  };
+}
 
 export default async function GroupPage({
   params,
