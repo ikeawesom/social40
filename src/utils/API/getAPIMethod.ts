03fa1a6bf1b781712c573359c50e1abe1d467ce1
ikeawesom <ikelimjl@gmail.com>
2023-10-30 15:47:27 +0800

    added sub options

--- src/utils/API/getAPIMethod.ts
+++ src/utils/API/getAPIMethod.ts
@@ -1,5 +1,6 @@
 export function getMethod(url: string) {
   const urlArray = url.split("/");
-  const method = urlArray[urlArray.length - 1];
-  return method;
+  const length = urlArray.length;
+  if (length === 7) return { option: urlArray[5], suboption: urlArray[6] };
+  return { suboption: urlArray[5] };
 }
