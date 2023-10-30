d9efdaa4c3d7503a11543c71f9a7a9c778ab8951
ikeawesom <ikelimjl@gmail.com>
2023-10-30 15:39:13 +0800

    fetching member data from server

--- app/(navigation)/groups/page.tsx
+++ app/(navigation)/groups/page.tsx
@@ -5,8 +5,6 @@ import LoadingScreenSmall from "@/src/components/screens/LoadingScreenSmall";
 import { dbHandler } from "@/src/firebase/db";
 import { MEMBER_SCHEMA } from "@/src/utils/schemas/members";
 import NotFoundScreen from "@/src/components/screens/NotFoundScreen";
-import OfflineScreen from "@/src/components/screens/OfflineScreen";
-import ServerErrorScreen from "@/src/components/screens/ServerErrorScreen";
 import { ROLES_HIERARCHY } from "@/src/utils/constants";
 import { GetPostObj } from "@/src/utils/API/GetPostObj";
 import { MemberGroupsType } from "@/src/utils/groups/getJoinedGroups";
@@ -14,6 +12,7 @@ import GroupsJoinedSection from "@/src/components/groups/GroupsJoinedSection";
 import { ownedGroupsType } from "@/src/utils/groups/getOwnedGroups";
 import GroupsCreatedSection from "@/src/components/groups/GroupsCreatedSection";
 import { Metadata } from "next";
+import ErrorScreenHandler from "@/src/utils/ErrorScreenHandler";
 
 export const metadata: Metadata = {
   title: "Groups",
@@ -29,31 +28,32 @@ export default async function Groups() {
     const memberID = data.value;
 
     try {
-      const res = await dbHandler.get({ col_name: "MEMBERS", id: memberID });
+      const PostObj = GetPostObj({
+        memberID: memberID,
+      });
+      // fetch member data from server
+      const res = await fetch(`${host}/api/profile/member`, PostObj);
+      const data = await res.json();
 
-      if (!res.status) throw new Error(res.error);
+      if (!data.status) throw new Error(data.error);
 
-      const memberData = res.data as MEMBER_SCHEMA;
+      const memberData = data.data as MEMBER_SCHEMA;
+
       const { role } = memberData;
       const admin = ROLES_HIERARCHY[role] >= ROLES_HIERARCHY["admin"];
 
       // get joined groups
-      const resA = await fetch(
-        `${host}/api/groups/joined`,
-        GetPostObj({ memberID })
-      );
+      const resA = await fetch(`${host}/api/groups/joined`, PostObj);
       const fetchedDataA = await resA.json();
       const joinedGroups = fetchedDataA.data as MemberGroupsType;
 
       var ownedGroups;
       if (admin) {
-        const resB = await fetch(
-          `${host}/api/groups/owned`,
-          GetPostObj({ memberID })
-        );
+        const resB = await fetch(`${host}/api/groups/owned`, PostObj);
         const fetchedDataB = await resB.json();
         ownedGroups = fetchedDataB.data as ownedGroupsType;
       }
+
       return (
         <>
           <HeaderBar text="Groups" />
@@ -64,10 +64,7 @@ export default async function Groups() {
         </>
       );
     } catch (err: any) {
-      const error = err.message;
-      if (error.includes("offline")) return <OfflineScreen />;
-      if (error.includes("not found")) return <NotFoundScreen />;
-      else return <ServerErrorScreen eMsg={error} />;
+      return ErrorScreenHandler(err);
     }
   }
   return <LoadingScreenSmall />;
