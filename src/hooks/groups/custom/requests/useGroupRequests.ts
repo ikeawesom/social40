36a2aa2d731945b533640f943e7a9d40995c382f
ikeawesom <ikelimjl@gmail.com>
2023-10-31 21:01:08 +0800

    fetch from API

--- src/hooks/groups/custom/requests/useGroupRequests.ts
+++ src/hooks/groups/custom/requests/useGroupRequests.ts
@@ -1,5 +1,6 @@
 "use client";
-import { dbHandler } from "@/src/firebase/db";
+import { useHostname } from "@/src/hooks/useHostname";
+import { GetPostObj } from "@/src/utils/API/GetPostObj";
 import { WAITLIST_SCHEMA } from "@/src/utils/schemas/waitlist";
 import { useState, useEffect } from "react";
 
@@ -8,6 +9,7 @@ export type WaitListData = {
 };
 
 export function useGroupRequests(groupID: string) {
+  const { host } = useHostname();
   const [requested, setRequested] = useState<WaitListData | null>();
   const [success, setSuccess] = useState(false);
 
@@ -20,20 +22,16 @@ export function useGroupRequests(groupID: string) {
 
   useEffect(() => {
     const handleFetch = async (groupID: string) => {
-      const res = await dbHandler.getDocs({
-        col_name: `GROUPS/${groupID}/WAITLIST`,
-      });
-      if (!res.status) return setRequested(null);
-      const waitlistArr = res.data as any;
-      var obj = {} as WaitListData;
-      var added = false;
-      waitlistArr.forEach((doc: any) => {
-        const data = doc.data() as WAITLIST_SCHEMA;
-        obj[data.memberID] = data;
-        if (!added) added = true;
-      });
-      if (added) setRequested(obj);
-      if (!added) setRequested(null);
+      const res = await fetch(
+        `${host}/api/groups/waitlist`,
+        GetPostObj({ sub: "get", groupID })
+      );
+      const fetchedData = await res.json();
+      console.log(fetchedData);
+      if (!fetchedData.status) return setRequested(null);
+
+      const { data } = fetchedData;
+      setRequested(data);
     };
 
     if (requested === undefined) handleFetch(groupID);
