74210073fac8bfe53c60957d4f2bb681857b8912
ikeawesom <ikelimjl@gmail.com>
2023-10-31 23:41:14 +0800

    fixed bug for roles

--- app/(navigation)/members/[memberID]/page.tsx
+++ app/(navigation)/members/[memberID]/page.tsx
@@ -3,11 +3,13 @@ import BookedStatus from "@/src/components/members/BookedStatus";
 import MemberBadges from "@/src/components/members/MemberBadges";
 import MemberPoints from "@/src/components/members/MemberPoints";
 import HeaderBar from "@/src/components/navigation/HeaderBar";
+import LoadingScreenSmall from "@/src/components/screens/LoadingScreenSmall";
 import RestrictedScreen from "@/src/components/screens/RestrictedScreen";
 import { GetPostObj } from "@/src/utils/API/GetPostObj";
 import ErrorScreenHandler from "@/src/utils/ErrorScreenHandler";
 import { ROLES_HIERARCHY } from "@/src/utils/constants";
 import { MEMBER_SCHEMA } from "@/src/utils/schemas/members";
+import { cookies } from "next/headers";
 import React from "react";
 
 export default async function MemberPage({
@@ -15,50 +17,74 @@ export default async function MemberPage({
 }: {
   params: { memberID: string };
 }) {
-  const memberID = params.memberID;
-  try {
-    const host = process.env.HOST;
+  const clickedMemberID = params.memberID;
+  const cookieStore = cookies();
 
-    const PostObj = GetPostObj({
-      memberID: memberID,
-    });
-    // fetch member data from server
-    const res = await fetch(`${host}/api/profile/member`, PostObj);
-    const data = await res.json();
+  const data = cookieStore.get("memberID");
+  if (data) {
+    const memberID = data.value;
+    try {
+      const host = process.env.HOST;
 
-    if (!data.status) throw new Error(data.error);
+      // fetch current member data from server
+      const PostObj = GetPostObj({
+        memberID,
+      });
 
-    const viewMemberData = data.data as MEMBER_SCHEMA;
-    const role = viewMemberData.role;
-    const rankName =
-      `${viewMemberData.rank} ${viewMemberData.displayName}`.trim();
-    const admin = ROLES_HIERARCHY[role] >= ROLES_HIERARCHY[viewMemberData.role];
+      const res = await fetch(`${host}/api/profile/member`, PostObj);
+      const data = await res.json();
 
-    if (!admin) return <RestrictedScreen />;
-    return (
-      <>
-        <HeaderBar text={memberID} back />
-        <div className="flex flex-col items-stretch justify-start gap-4">
-          <DefaultCard className="flex flex-col items-start justify-center gap-2">
-            <div className="flex w-full items-center justify-between">
-              <MemberPoints points={viewMemberData.points} />
-              <BookedStatus status={viewMemberData.bookedIn} />
-            </div>
-            <div className="flex flex-col items-start justify-center">
-              <h1 className="text-xl text-custom-dark-text">{rankName}</h1>
-              <p className="text-sm text-custom-grey-text">
-                {viewMemberData.memberID}
-              </p>
-              <p className="text-sm text-custom-grey-text">
-                Created on: {viewMemberData.createdOn}
-              </p>
-            </div>
-            <MemberBadges badges={viewMemberData.badges} />
-          </DefaultCard>
-        </div>
-      </>
-    );
-  } catch (err: any) {
-    return ErrorScreenHandler(err);
+      if (!data.status) throw new Error(data.error);
+
+      const currentMemberData = data.data as MEMBER_SCHEMA;
+      const { role } = currentMemberData;
+
+      // fetch clicked member data from server
+      const PostObjA = GetPostObj({
+        memberID: clickedMemberID,
+      });
+
+      const resA = await fetch(`${host}/api/profile/member`, PostObjA);
+      const dataA = await resA.json();
+
+      if (!dataA.status) throw new Error(dataA.error);
+
+      const viewMemberData = dataA.data as MEMBER_SCHEMA;
+      const viewMemberRole = viewMemberData.role;
+
+      const admin = ROLES_HIERARCHY[role] >= ROLES_HIERARCHY[viewMemberRole];
+
+      if (!admin) return <RestrictedScreen />;
+
+      const rankName =
+        `${viewMemberData.rank} ${viewMemberData.displayName}`.trim();
+
+      return (
+        <>
+          <HeaderBar text={clickedMemberID} back />
+          <div className="flex flex-col items-stretch justify-start gap-4">
+            <DefaultCard className="flex flex-col items-start justify-center gap-2">
+              <div className="flex w-full items-center justify-between">
+                <MemberPoints points={viewMemberData.points} />
+                <BookedStatus status={viewMemberData.bookedIn} />
+              </div>
+              <div className="flex flex-col items-start justify-center">
+                <h1 className="text-xl text-custom-dark-text">{rankName}</h1>
+                <p className="text-sm text-custom-grey-text">
+                  {viewMemberData.memberID}
+                </p>
+                <p className="text-sm text-custom-grey-text">
+                  Created on: {viewMemberData.createdOn}
+                </p>
+              </div>
+              <MemberBadges badges={viewMemberData.badges} />
+            </DefaultCard>
+          </div>
+        </>
+      );
+    } catch (err: any) {
+      return ErrorScreenHandler(err);
+    }
   }
+  return <LoadingScreenSmall />;
 }
