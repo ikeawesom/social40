13aa7728189acfe54ace81c9dab7784924eaf950
ikeawesom <ikelimjl@gmail.com>
2023-10-31 21:05:52 +0800

    router to link

--- src/components/groups/custom/settings/SettingsSection.tsx
+++ src/components/groups/custom/settings/SettingsSection.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import DefaultCard from "@/src/components/DefaultCard";
 import PrimaryButton from "@/src/components/utils/PrimaryButton";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function SettingsSection({ groupID }: { groupID: string }) {
-  const router = useRouter();
   return (
     <DefaultCard className="w-full flex flex-col items-center justify-center gap-2">
       <p className="text-sm text-custom-primary text-center">
         Admin ID: {groupID}
       </p>
-      <PrimaryButton
-        onClick={() => {
-          router.push(`/groups/${groupID}/settings`, { scroll: false });
-        }}
-      >
-        Group Settings
-      </PrimaryButton>
+      <Link href={`/groups/${groupID}/settings`} className="w-full">
+        <PrimaryButton>Group Settings</PrimaryButton>
+      </Link>
     </DefaultCard>
   );
 }
