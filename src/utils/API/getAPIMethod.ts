a7f2e3a52194068f0d1e0a9f9aea4db54e621d39
ikeawesom <ikelimjl@gmail.com>
2023-10-31 21:01:00 +0800

    api option

--- src/utils/API/getAPIMethod.ts
+++ src/utils/API/getAPIMethod.ts
@@ -1,6 +1,5 @@
 export function getMethod(url: string) {
   const urlArray = url.split("/");
   const length = urlArray.length;
-  if (length === 7) return { option: urlArray[5], suboption: urlArray[6] };
   return { option: urlArray[5] };
 }
