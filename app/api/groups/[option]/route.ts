4173101b79212e3d36d17eeecf3f52fdd1355002
ikeawesom <ikelimjl@gmail.com>
2023-10-31 21:01:36 +0800

    fetching from waitlist

--- app/api/groups/[option]/route.ts
+++ app/api/groups/[option]/route.ts
@@ -1,8 +1,10 @@
 import { dbHandler } from "@/src/firebase/db";
+import { WaitListData } from "@/src/hooks/groups/custom/requests/useGroupRequests";
 import { getMethod } from "@/src/utils/API/getAPIMethod";
 import { getJoinedGroups } from "@/src/utils/groups/getJoinedGroups";
 import { getOwnedGroups } from "@/src/utils/groups/getOwnedGroups";
 import { GROUP_MEMBERS_SCHEMA, GROUP_SCHEMA } from "@/src/utils/schemas/groups";
+import { WAITLIST_SCHEMA } from "@/src/utils/schemas/waitlist";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
@@ -44,6 +46,36 @@ export async function POST(request: NextRequest) {
 
     const groupData = res.data as GROUP_SCHEMA;
     return NextResponse.json({ status: true, data: groupData });
+  } else if (option === "waitlist") {
+    // manage waitlist methods
+    const { sub } = fetchedData;
+    const { groupID } = fetchedData;
+
+    if (sub === "get") {
+      // fetch all members from waitlist
+      const res = await dbHandler.getDocs({
+        col_name: `GROUPS/${groupID}/WAITLIST`,
+      });
+
+      if (!res.status)
+        return NextResponse.json({
+          status: false,
+          error: res.error,
+        });
+
+      const waitlistArr = res.data as any;
+      var obj = {} as WaitListData;
+      var empty = waitlistArr.length === 0;
+
+      if (empty) return NextResponse.json({ status: true, data: null });
+
+      waitlistArr.forEach((doc: any) => {
+        const data = doc.data() as WAITLIST_SCHEMA;
+        obj[data.memberID] = data;
+      });
+
+      return NextResponse.json({ status: true, data: obj });
+    }
   }
   return NextResponse.json({
     status: false,
