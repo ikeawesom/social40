668d4ca1d3f02fe20a7f35fba48c4a4e83a73123
ikeawesom <ikelimjl@gmail.com>
2023-11-01 08:26:13 +0800

    redirect upon signout

--- src/components/utils/SignoutButton.tsx
+++ src/components/utils/SignoutButton.tsx
@@ -6,6 +6,7 @@ import { getAuth } from "firebase/auth";
 import { FIREBASE_APP } from "@/src/firebase/config";
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
+import { useRouter } from "next/navigation";
 
 export default function SignoutButton({
   height,
@@ -16,10 +17,14 @@ export default function SignoutButton({
   width?: number;
   absolute?: boolean;
 }) {
+  const router = useRouter();
   const handleSignout = async () => {
     const auth = getAuth(FIREBASE_APP);
     const res = await authHandler.signOutUser(auth);
     if (!res.status) toast.error(res.data);
+    else {
+      router.push(`/auth?${new URLSearchParams({ new_user: "false" })}`);
+    }
   };
   return (
     <Image
