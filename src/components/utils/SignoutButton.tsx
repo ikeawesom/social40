2c5d61313db6a0808826a38748e45768c62a1097
ikeawesom <ikelimjl@gmail.com>
2023-11-01 08:17:52 +0800

    added customization

--- src/components/utils/SignoutButton.tsx
+++ src/components/utils/SignoutButton.tsx
@@ -5,8 +5,17 @@ import { toast } from "sonner";
 import { getAuth } from "firebase/auth";
 import { FIREBASE_APP } from "@/src/firebase/config";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
-export default function SignoutButton() {
+export default function SignoutButton({
+  height,
+  width,
+  absolute,
+}: {
+  height?: number;
+  width?: number;
+  absolute?: boolean;
+}) {
   const handleSignout = async () => {
     const auth = getAuth(FIREBASE_APP);
     const res = await authHandler.signOutUser(auth);
@@ -15,11 +24,14 @@ export default function SignoutButton() {
   return (
     <Image
       src="/icons/icon_signout.svg"
-      width={35}
-      height={35}
+      width={height ? height : 35}
+      height={height ? height : 35}
       alt="Sign Out"
       onClick={handleSignout}
-      className="rounded-full cursor-pointer absolute top-2 right-2"
+      className={twMerge(
+        "rounded-full cursor-pointer",
+        absolute ? "absolute top-2 right-2" : ""
+      )}
     />
   );
 }
