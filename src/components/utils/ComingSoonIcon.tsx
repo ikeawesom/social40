4398df8faec6802c0f0c9b6a7ed6e446edf009a9
ikeawesom <ikelimjl@gmail.com>
2023-11-01 11:47:35 +0800

    added customization

--- src/components/utils/ComingSoonIcon.tsx
+++ src/components/utils/ComingSoonIcon.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 export default function ComingSoonIcon({
   width,
   height,
+  small,
 }: {
   width?: number;
   height?: number;
+  small?: boolean;
 }) {
   return (
     <div className="flex flex-col items-center justify-center gap-4">
@@ -16,7 +19,14 @@ export default function ComingSoonIcon({
         width={width ? width : 150}
         height={height ? height : 150}
       />
-      <h1 className="text-custom-dark-text font-bold text-2xl">Coming soon!</h1>
+      <h1
+        className={twMerge(
+          "text-custom-dark-text font-bold",
+          small ? "text-lg" : "text-2xl"
+        )}
+      >
+        Coming soon!
+      </h1>
     </div>
   );
 }
