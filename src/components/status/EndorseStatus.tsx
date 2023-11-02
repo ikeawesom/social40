f3676be28ac4e07be29954d6eec719ee2934baf5
ikeawesom <ikelimjl@gmail.com>
2023-11-02 14:39:13 +0800

    added customization

--- src/components/status/EndorseStatus.tsx
+++ src/components/status/EndorseStatus.tsx
@@ -5,17 +5,23 @@ import LoadingIcon from "../utils/LoadingIcon";
 import { useHostname } from "@/src/hooks/useHostname";
 import { GetPostObj } from "@/src/utils/API/GetPostObj";
 import { toast } from "sonner";
+import { twMerge } from "tailwind-merge";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export default function EndorseStatus({
   status,
   statusID,
   memberID,
   adminID,
+  className,
+  router,
 }: {
   statusID: string;
   status: boolean;
   memberID: string;
   adminID: string;
+  className?: string;
+  router?: AppRouterInstance;
 }) {
   const [statusNew, setStatusNew] = useState(status);
   const { host } = useHostname();
@@ -26,14 +32,17 @@ export default function EndorseStatus({
     try {
       const PostObj = GetPostObj({ statusID, memberID, adminID });
       await fetch(`${host}/api/profile/endorse-status`, PostObj);
-      setStatusNew(true);
+      if (router) router.refresh();
+      else setStatusNew(true);
+
+      toast.success("Status endorsed successfully.");
     } catch (err: any) {
       toast.error(err.message);
     }
     setLoading(false);
   };
   return (
-    <div className="flex-1 grid place-items-center">
+    <div className={twMerge("flex-1 grid place-items-center", className)}>
       {statusNew ? (
         <p className="font-semibold text-custom-orange text-center">Endorsed</p>
       ) : loading ? (
