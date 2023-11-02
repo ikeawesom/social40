a3046d8d23e53bf79ae62aaaa8418df2080a4b31
ikeawesom <ikelimjl@gmail.com>
2023-11-02 22:01:34 +0800

    updated hierarchy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,8 @@
 export const ROLES_HIERARCHY = {
-  owner: { rank: 7, title: "Level 7" },
-  admin: { rank: 6, title: "Level 6" },
-  com4: { rank: 5, title: "Level 5" },
-  com3: { rank: 4, title: "Level 4" },
-  com2: { rank: 3, title: "Level 3" },
-  com1: { rank: 2, title: "Level 2" },
-  member: { rank: 2, title: "Level 1" },
+  owner: { rank: 4, title: "Level 4" },
+  admin: { rank: 3, title: "Level 3" },
+  commander: { rank: 2, title: "Level 2" },
+  member: { rank: 1, title: "Level 1" },
 } as { [role: string]: { rank: number; title: string } };
 
 export const BADGE_COLORS = {
