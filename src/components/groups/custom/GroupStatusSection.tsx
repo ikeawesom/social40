8c94fb19e741454a5d81df74c34ff0f9e9317015
ikeawesom <ikelimjl@gmail.com>
2023-11-02 23:31:34 +0800

    added condition to display group statuses

--- src/components/groups/custom/GroupStatusSection.tsx
+++ src/components/groups/custom/GroupStatusSection.tsx
@@ -17,58 +17,76 @@ export default function GroupStatusSection({
   GroupStatusList: GroupStatusType;
   adminID: string;
 }) {
+  var empty = true;
+
+  const keys = Object.keys(GroupStatusList);
+
+  for (var i = 0; i < keys.length; i++) {
+    const length = Object.keys(GroupStatusList[keys[i]]).length;
+    if (length !== 0) {
+      empty = false;
+      break;
+    }
+  }
+
   return (
     <DefaultCard className="w-full flex flex-col items-start justify-start gap-2 max-h-[80vh]">
       <div className="w-full">
         <h1 className="text-custom-dark-text font-semibold flex gap-1 items-center justify-start text-start">
           Group Statuses
         </h1>
-        <HRow />
+        <HRow className="mb-0" />
       </div>
-      <InnerContainer className="max-h-[80vh]">
-        {Object.keys(GroupStatusList).map((memberID: string) => {
-          const memberStatus = GroupStatusList[memberID];
-          const memberEmpty = Object.keys(memberStatus).length === 0;
-          if (!memberEmpty)
-            return (
-              <div
-                key={memberID}
-                className="w-full flex-col flex items-start justify-center"
-              >
-                <h1 className="p-2 font-semibold">{memberID}</h1>
-                <HRow className="my-0" />
-                {Object.keys(memberStatus).map((statusID: string) => {
-                  const statusData = memberStatus[statusID];
-                  const active = getActiveStatus(statusData.endDate);
+      {empty ? (
+        <p className="text-sm text-custom-grey-text">
+          Looks like nobody in this groups have statuses recorded.
+        </p>
+      ) : (
+        <InnerContainer className="max-h-[80vh]">
+          {Object.keys(GroupStatusList).map((memberID: string) => {
+            const memberStatus = GroupStatusList[memberID];
+            const memberEmpty = Object.keys(memberStatus).length === 0;
+            if (!memberEmpty)
+              return (
+                <div
+                  key={memberID}
+                  className="w-full flex-col flex items-start justify-center"
+                >
+                  <h1 className="p-2 font-semibold">{memberID}</h1>
+                  <HRow className="my-0" />
+                  {Object.keys(memberStatus).map((statusID: string) => {
+                    const statusData = memberStatus[statusID];
+                    const active = getActiveStatus(statusData.endDate);
 
-                  if (active)
-                    return (
-                      <MemberStatusTab
-                        active={active}
-                        adminID={adminID}
-                        memberID={memberID}
-                        statusData={statusData}
-                      />
-                    );
-                })}
-                {Object.keys(memberStatus).map((statusID: string) => {
-                  const statusData = memberStatus[statusID];
+                    if (active)
+                      return (
+                        <MemberStatusTab
+                          active={active}
+                          adminID={adminID}
+                          memberID={memberID}
+                          statusData={statusData}
+                        />
+                      );
+                  })}
+                  {Object.keys(memberStatus).map((statusID: string) => {
+                    const statusData = memberStatus[statusID];
 
-                  const active = getActiveStatus(statusData.endDate);
-                  if (!active)
-                    return (
-                      <MemberStatusTab
-                        active={active}
-                        adminID={adminID}
-                        memberID={memberID}
-                        statusData={statusData}
-                      />
-                    );
-                })}
-              </div>
-            );
-        })}
-      </InnerContainer>
+                    const active = getActiveStatus(statusData.endDate);
+                    if (!active)
+                      return (
+                        <MemberStatusTab
+                          active={active}
+                          adminID={adminID}
+                          memberID={memberID}
+                          statusData={statusData}
+                        />
+                      );
+                  })}
+                </div>
+              );
+          })}
+        </InnerContainer>
+      )}
     </DefaultCard>
   );
 }
