80651bcc0cbd4e4394a855d31062c3a17abac2b3
ikeawesom <ikelimjl@gmail.com>
2023-11-02 22:46:30 +0800

    added conditional permissions section

--- app/(navigation)/members/[memberID]/page.tsx
+++ app/(navigation)/members/[memberID]/page.tsx
@@ -2,11 +2,12 @@ import DefaultCard from "@/src/components/DefaultCard";
 import BookedStatus from "@/src/components/members/BookedStatus";
 import MemberBadges from "@/src/components/members/MemberBadges";
 import MemberPoints from "@/src/components/members/MemberPoints";
+import PermissionForm from "@/src/components/members/PermissionForm";
 import HeaderBar from "@/src/components/navigation/HeaderBar";
 import { StatusListType } from "@/src/components/profile/StatsSection";
 import StatusFeed from "@/src/components/profile/stats/StatusFeed";
-import RestrictedScreen from "@/src/components/screens/RestrictedScreen";
 import SignInAgainScreen from "@/src/components/screens/SignInAgainScreen";
+import HRow from "@/src/components/utils/HRow";
 import { GetPostObj } from "@/src/utils/API/GetPostObj";
 import ErrorScreenHandler from "@/src/utils/ErrorScreenHandler";
 import { ROLES_HIERARCHY } from "@/src/utils/constants";
@@ -63,12 +64,12 @@ export default async function MemberPage({
       if (!dataA.status) throw new Error(dataA.error);
 
       const viewMemberData = dataA.data as MEMBER_SCHEMA;
-      const viewMemberRole = viewMemberData.role;
 
-      const admin =
-        ROLES_HIERARCHY[role].rank >= ROLES_HIERARCHY[viewMemberRole].rank;
+      const permission =
+        ROLES_HIERARCHY[role].rank >= ROLES_HIERARCHY["commander"].rank;
 
-      if (!admin) return <RestrictedScreen />;
+      const higher =
+        ROLES_HIERARCHY[role].rank >= ROLES_HIERARCHY[viewMemberData.role].rank;
 
       const rankName =
         `${viewMemberData.rank} ${viewMemberData.displayName}`.trim();
@@ -84,26 +85,43 @@ export default async function MemberPage({
       return (
         <>
           <HeaderBar text={clickedMemberID} back />
-          <div className="flex flex-col items-stretch justify-start gap-4">
-            <DefaultCard className="flex flex-col items-start justify-center gap-2">
-              <div className="flex w-full items-center justify-between">
-                <MemberPoints points={viewMemberData.points} />
-                <BookedStatus status={viewMemberData.bookedIn} />
-              </div>
-              <div className="flex flex-col items-start justify-center">
-                <h1 className="text-xl text-custom-dark-text">{rankName}</h1>
-                <p className="text-sm text-custom-grey-text">
-                  {viewMemberData.memberID}
-                </p>
-                <p className="text-sm text-custom-grey-text">
-                  Created on: {viewMemberData.createdOn}
-                </p>
-              </div>
-              <MemberBadges badges={viewMemberData.badges} />
-            </DefaultCard>
-            <DefaultCard className="w-full">
-              <StatusFeed viewProfile status={statusList} />
-            </DefaultCard>
+          <div className="grid place-items-center">
+            <div className="flex flex-col items-stretch justify-start gap-4 max-w-[500px] w-full">
+              <DefaultCard className="flex flex-col items-start justify-center gap-2">
+                <div className="flex w-full items-center justify-between">
+                  <MemberPoints points={viewMemberData.points} />
+                  <BookedStatus status={viewMemberData.bookedIn} />
+                </div>
+                <div className="flex flex-col items-start justify-center">
+                  <h1 className="text-xl text-custom-dark-text">{rankName}</h1>
+                  <p className="text-sm text-custom-grey-text">
+                    {viewMemberData.memberID}
+                  </p>
+                  <p className="text-sm text-custom-grey-text">
+                    Created on: {viewMemberData.createdOn}
+                  </p>
+                </div>
+                <MemberBadges badges={viewMemberData.badges} />
+              </DefaultCard>
+              {permission && (
+                <DefaultCard className="w-full">
+                  <StatusFeed viewProfile status={statusList} />
+                </DefaultCard>
+              )}
+              {higher && (
+                <DefaultCard className="w-full">
+                  <h1 className="text-start font-semibold text-base">
+                    Permissions
+                  </h1>
+                  <HRow className="mb-2" />
+
+                  <PermissionForm
+                    currentMember={currentMemberData}
+                    viewMember={viewMemberData}
+                  />
+                </DefaultCard>
+              )}
+            </div>
           </div>
         </>
       );
