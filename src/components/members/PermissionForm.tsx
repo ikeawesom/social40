f075bde2ff4f63546fdb4a99d1e1226c6b25c634
ikeawesom <ikelimjl@gmail.com>
2023-11-03 09:02:28 +0800

    change to tier

--- src/components/members/PermissionForm.tsx
+++ src/components/members/PermissionForm.tsx
@@ -58,7 +58,7 @@ export default function PermissionForm({
     try {
       if (sameRole)
         throw new Error(
-          "You cannot change the permissions of another member of the same level."
+          "You cannot change the permissions of another member of the same tier."
         );
       const PostObj = GetPostObj({
         memberID: viewMember.memberID,
@@ -82,7 +82,7 @@ export default function PermissionForm({
         onClick={() => {
           sameRole &&
             toast.error(
-              "You cannot change the permissions of another member of the same level."
+              "You cannot change the permissions of another member of the same tier."
             );
         }}
       >
@@ -138,7 +138,7 @@ export default function PermissionForm({
                 onChange={handleChangeCfm}
               />
               <p className="text-sm text-custom-grey-text">
-                Note that the higher the level, the more permissions will be
+                Note that the higher the tier, the more permissions will be
                 available to this member.
               </p>
             </div>
