4887018cfc1a663d3ed6358aa345877af92a4acf
ikeawesom <ikelimjl@gmail.com>
2023-11-04 00:00:57 +0800

    renamed to activities

---
+++ src/components/groups/custom/GroupActivities.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ComingSoonCard from "../../utils/ComingSoonCard";
+
+export default function GroupActivities() {
+  return <ComingSoonCard text="Group Activities" />;
+}
