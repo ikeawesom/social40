f022f7f88cb3e6d772bdcabecbf9e9ffc63e2313
Ike <ikelimjl@gmail.com>
2023-11-04 16:21:20 +0800

    added auth error page

---
+++ app/(auth)/auth-error/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import SignInAgainScreen from "@/src/components/screens/SignInAgainScreen";
+
+export default function AuthErrorPage() {
+  return <SignInAgainScreen />;
+}
