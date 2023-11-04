84294b73a3755980f63e6d7fe3c6704798a9d963
Ike <ikelimjl@gmail.com>
2023-11-04 17:04:31 +0800

    added conditional render

--- app/(auth)/auth-error/page.tsx
+++ app/(auth)/auth-error/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import SignInAgainScreen from "@/src/components/screens/SignInAgainScreen";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default function AuthErrorPage() {
+  const cookieStore = cookies();
+  const data = cookieStore.get("memberID");
+  if (data) redirect("/home");
   return <SignInAgainScreen />;
 }
