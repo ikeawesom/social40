c3ddcedd88594115650cfbbf1dcdc011552356ee
Ike <ikelimjl@gmail.com>
2023-11-04 17:13:27 +0800

    added customization

--- src/components/utils/SignoutButton.tsx
+++ src/components/utils/SignoutButton.tsx
@@ -8,13 +8,18 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import SecondaryButton from "./SecondaryButton";
 import { LoadingIconBright } from "./LoadingIcon";
+import { twMerge } from "tailwind-merge";
 
 export default function SignoutButton({
   height,
   width,
+  textStyle,
+  text,
 }: {
   height?: number;
   width?: number;
+  textStyle?: string;
+  text?: string;
 }) {
   const [loading, setLoading] = useState(false);
   const router = useRouter();
@@ -39,7 +44,14 @@ export default function SignoutButton({
         <LoadingIconBright width={20} height={20} />
       ) : (
         <div className="flex items-center justify-center gap-2">
-          <p className="font-semibold text-custom-light-text">Sign out</p>
+          <p
+            className={twMerge(
+              "font-semibold text-custom-light-text",
+              textStyle
+            )}
+          >
+            {text ? text : "Sign out"}
+          </p>
           <Image
             src="/icons/icon_signout.svg"
             width={width ? width : 20}
