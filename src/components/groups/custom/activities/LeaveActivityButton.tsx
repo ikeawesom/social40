2423907930b86892b589fed35e7abf4f0f52f0a9
ikeawesom <ikelimjl@gmail.com>
2023-11-11 22:26:19 +0800

    added leave activity button

---
+++ src/components/groups/custom/activities/LeaveActivityButton.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React, { useState } from "react";
+import PrimaryButton from "@/src/components/utils/PrimaryButton";
+import { LoadingIconBright } from "@/src/components/utils/LoadingIcon";
+import { twMerge } from "tailwind-merge";
+import { toast } from "sonner";
+import { GetPostObj } from "@/src/utils/API/GetPostObj";
+import { useHostname } from "@/src/hooks/useHostname";
+import { useRouter } from "next/navigation";
+
+export default function LeaveActivityButton({
+  activityID,
+  memberID,
+  owner,
+}: {
+  activityID: string;
+  memberID: string;
+  owner: boolean;
+}) {
+  const router = useRouter();
+  const { host } = useHostname();
+  const [loading, setLoading] = useState(false);
+  const setAlert = async () => {
+    if (
+      confirm(
+        "Are you sure you want to leave this activity? You will have to request to participate again if you choose to leave."
+      )
+    )
+      await handleLeave();
+  };
+  const handleLeave = async () => {
+    setLoading(true);
+    try {
+      const ActivityObj = GetPostObj({ activityID, memberID });
+      const res = await fetch(`${host}/api/activity/group-leave`, ActivityObj);
+      const body = await res.json();
+      if (!body.status) throw new Error(body.error);
+      router.refresh();
+      router.back();
+      toast.success("Left group");
+    } catch (err: any) {
+      toast.error(err.message);
+    }
+    setLoading(false);
+  };
+  return (
+    <PrimaryButton
+      disabled={loading || owner}
+      type="submit"
+      onClick={setAlert}
+      className={twMerge("bg-custom-red grid place-items-center")}
+    >
+      {" "}
+      {owner ? (
+        "You cannot leave your own activity"
+      ) : loading ? (
+        <LoadingIconBright width={20} height={20} />
+      ) : (
+        "Leave activity"
+      )}
+    </PrimaryButton>
+  );
+}
