02a7bd4ee3767571ede6e8315ee897cef256f863
ikeawesom <ikelimjl@gmail.com>
2023-11-12 01:18:28 +0800

    toggle show requests

--- src/components/groups/custom/activities/ActivityWaitlist.tsx
+++ src/components/groups/custom/activities/ActivityWaitlist.tsx
@@ -13,6 +13,7 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { toast } from "sonner";
+import Image from "next/image";
 
 export type ActivityWaitlistType = {
   [memberID: string]: GROUP_ACTIVITY_WAITLIST;
@@ -25,6 +26,7 @@ export default function ActivityWaitlist({
   activityID: string;
 }) {
   const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
   const router = useRouter();
   const { host } = useHostname();
 
@@ -63,52 +65,70 @@ export default function ActivityWaitlist({
   return (
     <DefaultCard className="w-full flex flex-col items-start justify-center gap-2 ">
       <div className="w-full flex flex-col items-start justify-center">
-        <h1 className="text-custom-dark-text font-semibold">
-          Requesting to Participate ( {Object.keys(requestsData).length} )
-        </h1>
+        <div className="flex items-center justify-between w-full">
+          <h1 className="text-custom-dark-text font-semibold flex gap-1 items-center justify-start text-start">
+            Requests
+            <span className="bg-custom-red text-custom-light-text font-medium px-2 rounded-full text-sm text-center my-2">
+              {Object.keys(requestsData).length > 9
+                ? "9+"
+                : Object.keys(requestsData).length}
+            </span>
+          </h1>
+
+          <Image
+            onClick={() => setShow(!show)}
+            src="/icons/icon_arrow-down.svg"
+            alt="Show"
+            width={30}
+            height={30}
+            className={`duration-300 ease-in-out ${show ? "rotate-180" : ""}`}
+          />
+        </div>
         <HRow />
       </div>
-      <InnerContainer className="w-full">
-        {loading && (
-          <div className="w-full absolute grid place-items-center h-full bg-black/25 z-30">
-            <LoadingIcon width={30} height={30} />
-          </div>
-        )}
-        {Object.keys(requestsData).map((memberID: string) => {
-          const date = requestsData[memberID].dateRequested;
-          const dateStr = TimestampToDateString(date);
-          return (
-            <div
-              key={memberID}
-              className="w-full flex flex-col items-start justify-center py-2 px-3 duration-200 hover:bg-custom-light-text"
-            >
-              <Link
-                href={`/members/${memberID}`}
-                className="text-custom-dark-text font-semibold hover:opacity-70 duration-200"
-              >
-                {memberID}
-              </Link>
-              <h4 className="text-custom-grey-text text-sm">
-                Requested on: {dateStr}
-              </h4>
-              <div className="w-full flex items-center justify-between gap-2 mt-2">
-                <PrimaryButton
-                  className="py-1 border-[1px] border-transparent"
-                  onClick={() => handleAccept(memberID)}
-                >
-                  Accept
-                </PrimaryButton>
-                <SecondaryButton
-                  className="border-custom-red text-custom-red py-1"
-                  onClick={() => handleReject(memberID)}
-                >
-                  Reject
-                </SecondaryButton>
-              </div>
+      {show && (
+        <InnerContainer className="w-full">
+          {loading && (
+            <div className="w-full absolute grid place-items-center h-full bg-black/25 z-30">
+              <LoadingIcon width={30} height={30} />
             </div>
-          );
-        })}
-      </InnerContainer>
+          )}
+          {Object.keys(requestsData).map((memberID: string) => {
+            const date = requestsData[memberID].dateRequested;
+            const dateStr = TimestampToDateString(date);
+            return (
+              <div
+                key={memberID}
+                className="w-full flex flex-col items-start justify-center py-2 px-3 duration-200 hover:bg-custom-light-text"
+              >
+                <Link
+                  href={`/members/${memberID}`}
+                  className="text-custom-dark-text font-semibold hover:opacity-70 duration-200"
+                >
+                  {memberID}
+                </Link>
+                <h4 className="text-custom-grey-text text-sm">
+                  Requested on: {dateStr}
+                </h4>
+                <div className="w-full flex items-center justify-between gap-2 mt-2">
+                  <PrimaryButton
+                    className="py-1 border-[1px] border-transparent"
+                    onClick={() => handleAccept(memberID)}
+                  >
+                    Accept
+                  </PrimaryButton>
+                  <SecondaryButton
+                    className="border-custom-red text-custom-red py-1"
+                    onClick={() => handleReject(memberID)}
+                  >
+                    Reject
+                  </SecondaryButton>
+                </div>
+              </div>
+            );
+          })}
+        </InnerContainer>
+      )}
     </DefaultCard>
   );
 }
