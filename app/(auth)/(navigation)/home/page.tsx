6b5c7672635f38fb388604011991c387fc483c51
ikeawesom <ikelimjl@gmail.com>
2023-11-12 23:20:58 +0800

    added group activities feed

--- app/(auth)/(navigation)/home/page.tsx
+++ app/(auth)/(navigation)/home/page.tsx
@@ -1,14 +1,25 @@
-import HeaderBar from "@/src/components/navigation/HeaderBar";
+import FeedGroup from "@/src/components/feed/FeedGroup";
+import HomeHeaderBar from "@/src/components/navigation/HomeHeaderBar";
 import SignInAgainScreen from "@/src/components/screens/SignInAgainScreen";
-import ComingSoonCard from "@/src/components/utils/ComingSoonCard";
+import ComingSoonIcon from "@/src/components/utils/ComingSoonIcon";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { activity: string };
+}) {
+  const activityType = searchParams.activity;
+
+  if (!activityType)
+    redirect(`/home?${new URLSearchParams({ activity: "groups" })}`);
+
   const cookieStore = cookies();
   const data = cookieStore.get("memberID");
   if (data) {
@@ -16,11 +27,15 @@ export default async function Home() {
 
     return (
       <>
-        <HeaderBar text="Home" />
-        <div className="w-full grid place-items-center">
+        <HomeHeaderBar text="Social40" params={activityType} />
+        <div className="w-full grid place-items-center mt-[5.5rem]">
           <div className="flex flex-col w-full items-center justify-start gap-4 max-w-[500px]">
-            <ComingSoonCard text="Group Activities" />
-            <ComingSoonCard text="Friends Activities" />
+            {activityType === "groups" ? (
+              <FeedGroup memberID={memberID} />
+            ) : (
+              <ComingSoonIcon className="gap-2 mt-28" />
+              // <FeedFriends memberID={memberID}/>
+            )}
           </div>
         </div>
       </>
