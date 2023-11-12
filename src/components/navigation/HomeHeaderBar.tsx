eb5643865b94377b440a33e072c4ccc3c7303940
ikeawesom <ikelimjl@gmail.com>
2023-11-12 23:19:24 +0800

    header bar to toggle activity type

---
+++ src/components/navigation/HomeHeaderBar.tsx
@@ -0,0 +1,59 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { twMerge } from "tailwind-merge";
+import { MAX_LENGTH } from "@/src/utils/constants";
+import Link from "next/link";
+
+export default function HomeHeaderBar({
+  text,
+  params,
+}: {
+  text: string;
+  params: string;
+}) {
+  return (
+    <div
+      className={twMerge(
+        "w-full bg-white shadow-sm fixed top-0 left-0 flex items-center justify-start flex-col"
+      )}
+    >
+      <div className="flex items-center justify-center gap-x-2 z-40 w-full p-2">
+        <h1 className="font-semibold text-custom-dark-text text-2xl">
+          {text.length > MAX_LENGTH - 4
+            ? text.substring(0, MAX_LENGTH - 4 - 3) + "..."
+            : text}
+        </h1>
+        <Image
+          src="/pwa/icons/icon-512x512.png"
+          alt="Social40"
+          width={30}
+          height={30}
+        />
+      </div>
+      <div className="w-full items-center justify-between flex max-w-[500px]">
+        <Link
+          key="groups"
+          href={`/home?${new URLSearchParams({ activity: "groups" })}`}
+          className={twMerge(
+            "text-custom-grey-text font-semibold flex-1 px-4 py-2 border-b-2 text-center duration-200 hover:bg-custom-light-text",
+            params === "groups" && "border-b-custom-primary text-custom-primary"
+          )}
+        >
+          Groups
+        </Link>
+        <Link
+          key="friends"
+          href={`/home?${new URLSearchParams({ activity: "friends" })}`}
+          className={twMerge(
+            "text-custom-grey-text font-semibold flex-1 px-4 py-2 border-b-2 text-center duration-200 hover:bg-custom-light-text",
+            params === "friends" &&
+              "border-b-custom-primary text-custom-primary"
+          )}
+        >
+          Friends
+        </Link>
+      </div>
+    </div>
+  );
+}
