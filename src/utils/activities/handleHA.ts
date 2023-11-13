3ea2cd54431f689958d7b9e760c88642f58e1a3d
ikeawesom <ikelimjl@gmail.com>
2023-11-13 22:41:38 +0800

    utility function to handle HA

--- src/utils/activities/handleHA.ts
+++ src/utils/activities/handleHA.ts
@@ -1,18 +1,37 @@
-import getCurrentDate, { ActiveTimestamp } from "../getCurrentDate";
+import { Timestamp } from "firebase/firestore";
+import getCurrentDate, {
+  ActiveTimestamp,
+  CompareTimestamp,
+} from "../getCurrentDate";
 import { GROUP_ACTIVITY_SCHEMA } from "../schemas/group-activities";
+import handleResponses from "../handleResponses";
 
+function getLatestTimestamp(
+  today: Timestamp,
+  activitiesData: {
+    [activityID: string]: GROUP_ACTIVITY_SCHEMA;
+  }
+) {
+  let curTimestamp = today;
+  Object.keys(activitiesData).forEach((activitiyID: string) => {
+    const tempTimestamp = activitiesData[activitiyID].activityDate;
+    if (ActiveTimestamp(tempTimestamp)) return curTimestamp;
+    curTimestamp = tempTimestamp;
+  });
+  return curTimestamp;
+}
 export function handleHA(activitiesData: {
   [activityID: string]: GROUP_ACTIVITY_SCHEMA;
 }) {
   const empty = Object.keys(activitiesData).length === 0;
-  let HA = false;
+  let HA = handleResponses({ status: false });
 
   if (!empty) {
-    const curTimestamp = getCurrentDate();
-    const lastTimestamp =
-      activitiesData[Object.keys(activitiesData)[0]].activityDate;
-    // if activity hasn't begun
-    const active = ActiveTimestamp(lastTimestamp);
+    const today = getCurrentDate();
+    const curTimestamp = getLatestTimestamp(today, activitiesData);
+    const diff = CompareTimestamp(today, curTimestamp);
+    const daysDiff = Math.floor(diff / 24);
+    HA = handleResponses({ status: daysDiff <= 14, data: daysDiff });
   }
 
   return { empty, HA };
