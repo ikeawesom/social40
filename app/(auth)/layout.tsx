d63d5cd23b0ddd4548d18cc2871d56dc3a5c8542
ikeawesom <ikelimjl@gmail.com>
2023-11-19 18:54:57 +0800

    moved page wrapper to main app

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,10 @@
+import PageWrapper from "@/src/components/PageWrapper";
 import { AuthProvider } from "@/src/contexts/AuthContext";
 
 export default function NavLayout({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <PageWrapper>
+      <AuthProvider>{children}</AuthProvider>
+    </PageWrapper>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Toaster richColors position="top-center" />
-        <PageWrapper>{children}</PageWrapper>
+        {children}
       </body>
     </html>
   );
