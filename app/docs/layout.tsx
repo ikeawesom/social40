c1c9d5cedb0eefef0bc75fbf27e6801967bd532a
ikeawesom <ikelimjl@gmail.com>
2023-11-19 18:55:27 +0800

    custom docs layout

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -3,5 +3,5 @@ export default function DocsLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="bg-white">{children}</div>;
+  return <div className="bg-white w-full lg:px-28 md:px-8 p-4">{children}</div>;
 }
