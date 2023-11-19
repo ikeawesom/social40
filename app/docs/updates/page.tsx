91f09462b85e71f2b91fe50ce7e84dbfae47f5d0
ikeawesom <ikelimjl@gmail.com>
2023-11-19 18:43:14 +0800

    modified updates structure

--- app/docs/updates/page.tsx
+++ app/docs/updates/page.tsx
@@ -1,27 +1,27 @@
+import HRow from "@/src/components/utils/HRow";
 import { VERSION_MAP } from "@/src/utils/constants";
 import React from "react";
 
 export default function PatchNotesPage() {
   return (
     <div className="grid place-items-center w-full">
-      <div className="flex flex-col items-start justify-start gap-10 w-full max-w-[600px]">
+      <div className="flex flex-col items-start justify-start gap-10 w-full max-w-[600px] p-4">
         <h1 className="text-5xl text-custom-dark-text font-bold">
           Version Updates
         </h1>
-        {Object.keys(VERSION_MAP).map((version: string) => {
-          const updates = VERSION_MAP[version].updates;
+        {Object.keys(VERSION_MAP).map((versionNumber: string) => {
+          const { version, desc, title, updates } = VERSION_MAP[versionNumber];
           const emptyUpdates = updates.length === 0;
           return (
             <div
-              key={version}
+              key={versionNumber}
               className="flex flex-col w-full items-start justify-center"
             >
               <h1 className="text-custom-dark-text text-2xl font-semibold">
-                v{version}
+                v{version}: {title}
               </h1>
-              <p className="text-custom-dark-text">
-                {VERSION_MAP[version].desc}
-              </p>
+              <HRow />
+              <p className="text-custom-dark-text">{desc}</p>
               {!emptyUpdates && (
                 <ul className="mt-3 list-disc">
                   <h4 className="text-custom-dark-text text-lg">What's New?</h4>
