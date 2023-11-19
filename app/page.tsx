bca55fd6598611fb15c6cb2a477b88ff4a5c1719
ikeawesom <ikelimjl@gmail.com>
2023-11-19 18:55:06 +0800

    added page wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/src/components/Hero";
+import PageWrapper from "@/src/components/PageWrapper";
 import PrimaryButton from "@/src/components/utils/PrimaryButton";
 import InstallButton from "@/src/utils/InstallButton";
 import { VERSION_DESC, VERSION_NUMBER } from "@/src/utils/constants";
@@ -17,49 +18,51 @@ export default async function Home() {
   const data = cookieStore.get("memberID");
 
   return (
-    <div className="grid place-items-center h-[80vh]">
-      <div className="flex flex-col gap-y-4 items-center justify-center">
-        <Hero />
-        <div className="flex items-center justify-between gap-3 w-full max-[400px]:flex-col">
-          <InstallButton />
-          {!data ? (
-            <Link href="/auth" className="w-full flex-1">
-              <PrimaryButton className="flex items-center justify-center text-lg">
-                Get started
-                <Image
-                  src="/icons/icon_right_bright.svg"
-                  alt=""
-                  width={30}
-                  height={30}
-                />
-              </PrimaryButton>
+    <PageWrapper>
+      <div className="grid place-items-center h-[80vh]">
+        <div className="flex flex-col gap-y-4 items-center justify-center">
+          <Hero />
+          <div className="flex items-center justify-between gap-3 w-full max-[400px]:flex-col">
+            <InstallButton />
+            {!data ? (
+              <Link href="/auth" className="w-full flex-1">
+                <PrimaryButton className="flex items-center justify-center text-lg">
+                  Get started
+                  <Image
+                    src="/icons/icon_right_bright.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                </PrimaryButton>
+              </Link>
+            ) : (
+              <Link href="/home" className="w-full flex-1">
+                <PrimaryButton className="flex items-center justify-center text-lg">
+                  Go to home
+                  <Image
+                    src="/icons/icon_right_bright.svg"
+                    alt=""
+                    width={30}
+                    height={30}
+                  />
+                </PrimaryButton>
+              </Link>
+            )}
+          </div>
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <p className="text-custom-grey-text text-center text-sm">
+              v{VERSION_NUMBER}: {VERSION_DESC}
+            </p>
+            <Link
+              href="/docs/updates"
+              className="text-custom-primary font-semibold text-sm hover:underline duration-200"
+            >
+              View updates
             </Link>
-          ) : (
-            <Link href="/home" className="w-full flex-1">
-              <PrimaryButton className="flex items-center justify-center text-lg">
-                Go to home
-                <Image
-                  src="/icons/icon_right_bright.svg"
-                  alt=""
-                  width={30}
-                  height={30}
-                />
-              </PrimaryButton>
-            </Link>
-          )}
-        </div>
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <p className="text-custom-grey-text text-center text-sm">
-            v{VERSION_NUMBER}: {VERSION_DESC}
-          </p>
-          <Link
-            href="/docs/updates"
-            className="text-custom-primary font-semibold text-sm hover:underline duration-200"
-          >
-            View updates
-          </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </PageWrapper>
   );
 }
