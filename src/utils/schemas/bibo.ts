d7621bb72a0262b1e4a53a179c4ff3fc92d0a51e
ikeawesom <ikelimjl@gmail.com>
2023-11-22 15:13:28 +0800

    modified schema

--- src/utils/schemas/bibo.ts
+++ src/utils/schemas/bibo.ts
@@ -2,12 +2,10 @@ import { Timestamp } from "firebase/firestore";
 
 export type BIBO_DB_TYPE = { [date: string]: BIBO_SCHEMA };
 export type BIBO_SCHEMA = {
-  [time: string]: {
-    bookedInDate: string;
-    bookedInTime: string;
-    bookedIn: boolean;
-    memberID: string; // member ID
-    verifiedBy: string; // admin who booked member in
-    timestamp: Timestamp;
-  };
+  bookedInDate: string;
+  bookedInTime: string;
+  bookedIn: boolean;
+  memberID: string; // member ID
+  verifiedBy: string; // admin who booked member in
+  timestamp: Timestamp;
 };
