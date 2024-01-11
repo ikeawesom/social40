8e4ee958410bd3d377ee280eeaef5b6676f2f23a
ikeawesom <ikelimjl@gmail.com>
2024-01-12 02:42:59 +0800

    added custom styles

--- src/components/utils/QueryInput.tsx
+++ src/components/utils/QueryInput.tsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function QueryInput({
   search,
   handleSearch,
   placeholder,
+  className,
 }: {
   placeholder: string;
   search: string;
   handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 }) {
   return (
     <input
-      className="mb-2"
+      className={twMerge("mb-2", className)}
       value={search}
       onChange={handleSearch}
       placeholder={placeholder}
