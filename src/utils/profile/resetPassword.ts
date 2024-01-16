6573040f4c24922a65e819e0da359b8b3e975f27
ikeawesom <ikelimjl@gmail.com>
2024-01-16 23:10:41 +0800

    utility function to reset password

---
+++ src/utils/profile/resetPassword.ts
@@ -0,0 +1,34 @@
+import { authHandler } from "@/src/firebase/auth";
+import { FIREBASE_APP, SECONDARY_FIREBASE_APP } from "@/src/firebase/config";
+import { getAuth, updatePassword } from "firebase/auth";
+import handleResponses from "../handleResponses";
+
+export default async function resetPassword(
+  username: string,
+  password: string,
+  newPassword: string
+) {
+  const email = `${username}@digital40sar.com`;
+
+  try {
+    const auth = getAuth(SECONDARY_FIREBASE_APP);
+    const res = await authHandler.signIn(auth, email, password);
+    if (!res.status) throw new Error(res.error);
+
+    // change password
+    const selectedUser = auth.currentUser;
+    if (!selectedUser) throw new Error("User not found");
+    await updatePassword(selectedUser, newPassword);
+
+    const authA = getAuth(FIREBASE_APP);
+    const curUser = authA.currentUser;
+    if (curUser?.uid !== selectedUser?.uid) {
+      // sign out selected user
+      const resA = await authHandler.signOutUser(auth);
+      if (!resA.status) throw new Error(resA.error);
+    }
+    return handleResponses();
+  } catch (err: any) {
+    return handleResponses({ status: false, error: err.message });
+  }
+}
