5168ccfaf38acb315b38524aa56aa6c5220d8f0a
ikeawesom <ikelimjl@gmail.com>
2024-01-24 18:10:08 +0800

    combined status and members section to strength

--- src/components/groups/custom/GroupMembers.tsx
+++ src/components/groups/custom/GroupMembers.tsx
@@ -1,12 +1,12 @@
 "use client";
 import React, { useState } from "react";
-import DefaultCard from "../../DefaultCard";
 import InnerContainer from "../../utils/InnerContainer";
 import GroupMemberTab from "./GroupMemberTab";
 import { GROUP_MEMBERS_SCHEMA } from "@/src/utils/schemas/groups";
 import StatusDot from "../../utils/StatusDot";
 import useQueryObj from "@/src/hooks/useQueryObj";
 import QueryInput from "../../utils/QueryInput";
+import { twMerge } from "tailwind-merge";
 
 export type GroupDetailsType = {
   [memberID: string]: GROUP_MEMBERS_SCHEMA;
@@ -21,41 +21,46 @@ export default function GroupMembers({
   groupID: string;
   curMember: GROUP_MEMBERS_SCHEMA;
 }) {
+  const [show, setShow] = useState(false);
   const [online, setOnline] = useState(0);
   const { itemList, search, handleSearch } = useQueryObj({ obj: membersList });
 
-  const length = Object.keys(itemList).length;
-
   const addOnline = () => setOnline((online) => online + 1);
   return (
-    <DefaultCard className="w-full flex flex-col items-start justify-start gap-2 max-h-[80vh]">
-      <div className="w-full">
-        <div className="flex w-full items-center justify-between">
-          <h1 className="text-custom-dark-text font-semibold text-start">
-            Members ( {length} )
-          </h1>
-          <div className="flex items-center justify-end gap-1">
+    <div className="w-full flex flex-col items-start justify-start gap-2 max-h-[80vh]">
+      <h1
+        onClick={() => setShow(!show)}
+        className={twMerge(
+          "text-start cursor-pointer underline text-sm duration-150",
+          !show ? "text-custom-grey-text" : "text-custom-primary"
+        )}
+      >
+        {show ? "Hide Breakdown" : "View Breakdown"}
+      </h1>
+      {show && (
+        <>
+          <QueryInput
+            placeholder="Search for Member ID"
+            handleSearch={handleSearch}
+            search={search}
+          />
+          <div className="flex items-center justify-end gap-1 w-fit">
             <p className="text-xs text-custom-grey-text">{online} in camp</p>
             <StatusDot className="w-1 h-1" status />
           </div>
-        </div>
-      </div>
-      <QueryInput
-        placeholder="Search for Member ID"
-        handleSearch={handleSearch}
-        search={search}
-      />
-      <InnerContainer className="max-h-[90vh]">
-        {Object.keys(itemList).map((item) => (
-          <GroupMemberTab
-            addOnline={addOnline}
-            curMember={curMember}
-            groupID={groupID}
-            key={item}
-            data={itemList[item]}
-          />
-        ))}
-      </InnerContainer>
-    </DefaultCard>
+          <InnerContainer className="max-h-[90vh]">
+            {Object.keys(itemList).map((item) => (
+              <GroupMemberTab
+                addOnline={addOnline}
+                curMember={curMember}
+                groupID={groupID}
+                key={item}
+                data={itemList[item]}
+              />
+            ))}
+          </InnerContainer>
+        </>
+      )}
+    </div>
   );
 }
---
+++ src/components/groups/custom/GroupStrengthSection.tsx
@@ -0,0 +1,163 @@
+"use client";
+import React, { useState } from "react";
+import DefaultCard from "../../DefaultCard";
+import HRow from "../../utils/HRow";
+import { STATUS_SCHEMA } from "@/src/utils/schemas/statuses";
+import InnerContainer from "../../utils/InnerContainer";
+import MemberStatusTab from "./MemberStatusTab";
+import { ActiveTimestamp } from "@/src/utils/getCurrentDate";
+import { twMerge } from "tailwind-merge";
+import { GROUP_MEMBERS_SCHEMA } from "@/src/utils/schemas/groups";
+import GroupMembers, { GroupDetailsType } from "./GroupMembers";
+import { GROUP_ROLES_HEIRARCHY } from "@/src/utils/constants";
+
+export type GroupStatusType = {
+  [memberID: string]: { [statusID: string]: STATUS_SCHEMA };
+};
+
+export default function GroupStrengthSection({
+  GroupStatusList,
+  adminID,
+  curMember,
+  groupID,
+  membersList,
+}: {
+  GroupStatusList: GroupStatusType;
+  adminID: string;
+  membersList: GroupDetailsType;
+  groupID: string;
+  curMember: GROUP_MEMBERS_SCHEMA;
+}) {
+  const [show, setShow] = useState(false);
+  var empty = true;
+
+  const keys = Object.keys(GroupStatusList);
+
+  for (var i = 0; i < keys.length; i++) {
+    const length = Object.keys(GroupStatusList[keys[i]]).length;
+    if (length !== 0) {
+      empty = false;
+      break;
+    }
+  }
+
+  const totalStrength = Object.keys(membersList).length;
+  const commandersStrength = Object.keys(membersList).filter(
+    (memberID: string) => {
+      return (
+        GROUP_ROLES_HEIRARCHY[membersList[memberID].role].rank >=
+        GROUP_ROLES_HEIRARCHY["admin"].rank
+      );
+    }
+  ).length;
+
+  const mcLength = Object.keys(GroupStatusList).filter((memberID: string) => {
+    const statusObjList = GroupStatusList[memberID];
+    const mcList = Object.keys(statusObjList).filter((statusID: string) => {
+      const { endDate } = statusObjList[statusID];
+      return ActiveTimestamp(endDate) && statusObjList[statusID].mc === true;
+    });
+    return mcList.length > 0;
+  }).length;
+
+  const statusLength = Object.keys(GroupStatusList).filter(
+    (memberID: string) => {
+      const statusObjList = GroupStatusList[memberID];
+      const statusList = Object.keys(statusObjList).filter(
+        (statusID: string) => {
+          const { endDate } = statusObjList[statusID];
+          return (
+            ActiveTimestamp(endDate) && statusObjList[statusID].mc === false
+          );
+        }
+      );
+      return statusList.length > 0;
+    }
+  ).length;
+
+  return (
+    <DefaultCard className="w-full flex flex-col items-start justify-start gap-2">
+      <h1 className="text-custom-dark-text font-semibold">Strength</h1>
+      <h1 className="text-start text-custom-dark-text">
+        Total: <span className="font-bold">{totalStrength}</span>
+      </h1>
+      <h1 className="text-start text-custom-dark-text">
+        Commanders: <span className="font-bold">{commandersStrength}</span>
+      </h1>
+      <h1 className="text-start text-custom-dark-text">
+        Men:{" "}
+        <span className="font-bold">{totalStrength - commandersStrength}</span>
+      </h1>
+      <GroupMembers
+        curMember={curMember}
+        groupID={groupID}
+        membersList={membersList}
+      />
+      <HRow />
+      <h1 className="text-start text-custom-dark-text">
+        Statuses: <span className="font-bold">{statusLength}</span>
+      </h1>
+      <h1 className="text-start text-custom-dark-text">
+        MCs: <span className="font-bold">{mcLength}</span>
+      </h1>
+      <div className="w-full flex flex-col items-start justify-start gap-2 max-h-[80vh]">
+        <h1
+          onClick={() => setShow(!show)}
+          className={twMerge(
+            "text-start cursor-pointer underline text-sm duration-150",
+            !show ? "text-custom-grey-text" : "text-custom-primary"
+          )}
+        >
+          {show ? "Hide Breakdown" : "View Breakdown"}
+        </h1>
+        {show && (
+          <>
+            <InnerContainer
+              className={twMerge(
+                "min-h-[10vh] max-h-[100vh]",
+                empty &&
+                  "grid place-items-center justify-center overflow-hidden p-4"
+              )}
+            >
+              {empty ? (
+                <p className="text-sm text-custom-grey-text">
+                  Looks like nobody in this groups have statuses recorded.
+                </p>
+              ) : (
+                Object.keys(GroupStatusList).map((memberID: string) => {
+                  const memberStatus = GroupStatusList[memberID];
+                  const memberEmpty = Object.keys(memberStatus).length === 0;
+                  if (!memberEmpty)
+                    return (
+                      <div
+                        key={memberID}
+                        className="w-full flex-col flex items-start justify-center"
+                      >
+                        <h1 className="p-2 font-semibold text-sm">
+                          {memberID}
+                        </h1>
+                        <HRow className="my-0" />
+                        {Object.keys(memberStatus).map((statusID: string) => {
+                          const statusData = memberStatus[statusID];
+                          const active = ActiveTimestamp(statusData.endDate);
+
+                          return (
+                            <MemberStatusTab
+                              key={statusData.statusID}
+                              active={active}
+                              memberID={memberID}
+                              statusData={statusData}
+                            />
+                          );
+                        })}
+                      </div>
+                    );
+                })
+              )}
+            </InnerContainer>
+          </>
+        )}
+      </div>
+    </DefaultCard>
+  );
+}
