4d9c682d9bcd0bab541c3e67d0645886794596eb
ikeawesom <ikelimjl@gmail.com>
2024-01-30 14:02:18 +0800

    reduced transition speed

--- src/components/utils/PrimaryButton.tsx
+++ src/components/utils/PrimaryButton.tsx
@@ -17,7 +17,7 @@ export default function PrimaryButton(props: ButtonType) {
       disabled={props.disabled}
       type={props.type ? props.type : "button"}
       className={twMerge(
-        "shadow-sm text-sm duration-200 ease-in-out w-full rounded-lg bg-custom-primary text-custom-light-text px-6 py-2",
+        "shadow-sm text-sm duration-150 ease-in-out w-full rounded-lg bg-custom-primary text-custom-light-text px-6 py-2",
         `${
           props.disabled
             ? "opacity-70 cursor-not-allowed"
