f5f91fe03b58740ea96ebeffba6d44ffe3685e45
ikeawesom <ikelimjl@gmail.com>
2024-01-30 19:19:39 +0800

    reverted sign in again screen

--- app/(auth)/(navigation)/home/page.tsx
+++ app/(auth)/(navigation)/home/page.tsx
@@ -3,7 +3,6 @@ import FeedGroup from "@/src/components/feed/FeedGroup";
 import GroupsScrollSection from "@/src/components/feed/GroupsScrollSection";
 import HomeHeaderBar from "@/src/components/navigation/HomeHeaderBar";
 import ErrorActivities from "@/src/components/screens/ErrorActivities";
-import LoadingScreenSmall from "@/src/components/screens/LoadingScreenSmall";
 import SignInAgainScreen from "@/src/components/screens/SignInAgainScreen";
 import ComingSoonIcon from "@/src/components/utils/ComingSoonIcon";
 import { GetPostObj } from "@/src/utils/API/GetPostObj";
@@ -101,7 +100,7 @@ export default async function Home({
       </>
     );
   }
-  return <LoadingScreenSmall />;
+  return <SignInAgainScreen />;
 }
 // {groupsList.length !== 1 && (
 //                     <>
