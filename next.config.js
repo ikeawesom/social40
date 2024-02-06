2172d5dd0cf9b5840ebcb5df5d4b24c90efc8684
ikeawesom <ikelimjl@gmail.com>
2024-02-06 17:24:16 +0800

    firebase storage config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/v0/b/social40-15b92.appspot.com/o/**",
+      },
+    ],
+  },
+};
 const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   dest: "public/pwa/build",
