9704227f9fc45da3f1ecaf2ad1f8f4868a2e1b29
ikeawesom <ikelimjl@gmail.com>
2024-04-09 14:20:40 +0800

    created new announcement schemas

--- src/utils/schemas/announcements.ts
+++ src/utils/schemas/announcements.ts
@@ -1,9 +1,27 @@
 import { Timestamp } from "firebase/firestore";
 
+export const getDefaultAnnouncement = (memberID: string) => {
+  return {
+    title: "",
+    desc: "",
+    createdBy: memberID,
+    groups: [] as string[],
+    pin: false,
+  } as ANNOUNCEMENT_SCHEMA;
+};
+
 export type ANNOUNCEMENT_SCHEMA = {
-  announcementID: string;
+  announcementID?: string;
   title: string;
   desc: string;
+  pin?: boolean;
+  groups?: string[];
   createdBy: string; // memberID
-  createdOn: Timestamp;
+  createdOn?: Timestamp;
+};
+
+export type PostType = {
+  title: string;
+  desc: string;
+  createdBy: string;
 };
