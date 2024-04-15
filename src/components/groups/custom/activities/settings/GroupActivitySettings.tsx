dc355e2fa9aa691e273d1e8f14e059c34532e58b
ikeawesom <ikelimjl@gmail.com>
2024-04-15 15:42:04 +0800

    debug

--- src/components/groups/custom/activities/settings/GroupActivitySettings.tsx
+++ src/components/groups/custom/activities/settings/GroupActivitySettings.tsx
@@ -29,9 +29,10 @@ export default function GroupActivitySettings({
     TimestampToDateString(activityData.activityDate)
   );
 
-  const tempTimestamp = new Date(activityData.activityDate.seconds * 1000);
-  tempTimestamp.setHours(tempTimestamp.getHours() - 8);
-  const timestampStr = DateToString(tempTimestamp);
+  // const tempTimestamp = new Date(activityData.activityDate.seconds * 1000);
+  // tempTimestamp.setHours(tempTimestamp.getHours() - 8);
+  // const timestampStr = DateToString(tempTimestamp);
+  const timestampStr = TimestampToDateString(activityData.activityDate);
   const oldDate = timestampStr.split(" ")[0];
   const oldTime = timestampStr.split(" ")[1];
 
