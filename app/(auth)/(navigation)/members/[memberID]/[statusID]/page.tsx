ad404f225b0780763a3ea1c7d1b7a603766c8b4d
ikeawesom <ikelimjl@gmail.com>
2024-04-15 16:23:49 +0800

    debug

--- app/(auth)/(navigation)/members/[memberID]/[statusID]/page.tsx
+++ app/(auth)/(navigation)/members/[memberID]/[statusID]/page.tsx
@@ -109,7 +109,6 @@ export default async function CustomStatusPage({
                 <p className="text-custom-grey-text text-sm">
                   End Date: {TimestampToDateString(endDate).split(" ")[0]}
                 </p>
-                <p>debug: {TimestampToDateString(endDate)}</p>
               </div>
             </DefaultCard>
             <ActiveStatusSection active={active} />
