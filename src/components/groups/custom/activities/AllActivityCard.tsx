ed33cc0aca804a196940298d56cd74c22032689c
ikeawesom <ikelimjl@gmail.com>
2024-04-23 04:42:24 +0800

    general activities styling

--- src/components/groups/custom/activities/AllActivityCard.tsx
+++ src/components/groups/custom/activities/AllActivityCard.tsx
@@ -19,23 +19,25 @@ export default function AllActivityCard({
   const active = ActiveTimestamp(activityDate);
 
   return (
-    <DefaultCard className="w-full flex flex-col items-start justify-start">
-      <div className="w-full flex items-center justify-between flex-wrap gap-x-2">
-        <Link
-          href={`/groups/${groupID}/activity?${new URLSearchParams({
-            id: activityID,
-          })}`}
-          className="text-start font-semibold text-lg text-custom-dark-text duration-200 hover:opacity-70"
-        >
-          {activityTitle}
-        </Link>
-        <ActivityStatusTab active={active} />
-      </div>
-      <p className="text-custom-dark-text text-sm">{activityDesc}</p>
-      <p className="text-custom-grey-text text-xs mt-1">
-        {active ? "Begins on: " : "Ended on: "}
-        {dateStr}
-      </p>
-    </DefaultCard>
+    <Link
+      className="w-full"
+      href={`/groups/${groupID}/activity?${new URLSearchParams({
+        id: activityID,
+      })}`}
+    >
+      <DefaultCard className="w-full flex flex-col items-start justify-start duration-150 hover:bg-white/50">
+        <div className="w-full flex items-center justify-between flex-wrap gap-x-2">
+          <h1 className="text-start font-semibold text-lg text-custom-dark-text">
+            {activityTitle}
+          </h1>
+          <ActivityStatusTab active={active} />
+        </div>
+        <p className="text-custom-dark-text text-sm">{activityDesc}</p>
+        <p className="text-custom-grey-text text-xs mt-1">
+          {active ? "Begins on: " : "Ended on: "}
+          {dateStr}
+        </p>
+      </DefaultCard>
+    </Link>
   );
 }
