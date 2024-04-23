d4364265e69aa903a8b314e53e8939aa15310845
ikeawesom <ikelimjl@gmail.com>
2024-04-23 08:29:54 +0800

    custom hook to manage state of HA details

---
+++ src/hooks/groups/custom/useHADetails.ts
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export function useHADetails() {
+  const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
+  const [start, setStart] = useState({
+    day: "01",
+    month: "01",
+    year: "2024",
+  });
+
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setStart({ ...start, [e.target.name]: e.target.value });
+  };
+
+  const enable = () => setShow(true);
+  const disable = () => setShow(false);
+  const toggleLoad = () => setLoading(!loading);
+
+  return {
+    onChange,
+    enable,
+    disable,
+    show,
+    start,
+    loading,
+    toggleLoad,
+  };
+}
