1199ec1340377fc5e0dbc5ec3ea1884b2cad7682
ikeawesom <ikelimjl@gmail.com>
2024-04-26 18:28:20 +0800

    modified loading page styles

--- src/components/screens/LoadingScreen.tsx
+++ src/components/screens/LoadingScreen.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import LoadingIcon from "../utils/LoadingIcon";
 import { twMerge } from "tailwind-merge";
 
-export default function LoadingScreen({ className }: { className?: string }) {
+export default function LoadingScreen({
+  className,
+  text,
+}: {
+  className?: string;
+  text?: string;
+}) {
   return (
     <div
       className={twMerge("h-[85vh] grid place-items-center m-0 p-0", className)}
     >
-      <LoadingIcon />
+      <div className="flex items-center justify-center gap-4 flex-col p-6">
+        <LoadingIcon />
+        {text && (
+          <h1 className="text-center text-sm text-custom-dark-text">{text}</h1>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/screens/LoadingScreenSmall.tsx
+++ src/components/screens/LoadingScreenSmall.tsx
@@ -4,13 +4,20 @@ import LoadingIcon from "../utils/LoadingIcon";
 export default function LoadingScreenSmall({
   width,
   height,
+  text,
 }: {
   width?: number;
   height?: number;
+  text?: string;
 }) {
   return (
     <div className="grid place-items-center w-full h-[40vh]">
-      <LoadingIcon height={height ? height : 80} width={width ? width : 80} />
+      <div className="flex items-center justify-center gap-4 flex-col p-6">
+        <LoadingIcon height={height ? height : 80} width={width ? width : 80} />
+        {text && (
+          <h1 className="text-center text-sm text-custom-dark-text">{text}</h1>
+        )}
+      </div>
     </div>
   );
 }
