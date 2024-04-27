45e3247d9b02d9e3e2b108fab9475e1cc27c1c15
Ike <ikelimjl@gmail.com>
2024-04-27 18:08:41 +0800

    separated multiple kick from scroll

--- src/components/groups/custom/activities/participants/ParticipantContainer.tsx
+++ src/components/groups/custom/activities/participants/ParticipantContainer.tsx
@@ -1,5 +1,5 @@
+import { handleReload } from "@/src/components/navigation/HeaderBar";
 import FormInputContainer from "@/src/components/utils/FormInputContainer";
-import HRow from "@/src/components/utils/HRow";
 import InnerContainer from "@/src/components/utils/InnerContainer";
 import LoadingIcon from "@/src/components/utils/LoadingIcon";
 import SecondaryButton from "@/src/components/utils/SecondaryButton";
@@ -76,7 +76,7 @@ export default function ParticipantContainer({
         if (!item.status) throw new Error(item.error);
       });
       toast.success("Kicked members.");
-      router.refresh();
+      handleReload(router);
     } catch (err: any) {
       toast.error(err.message);
     }
@@ -104,113 +104,114 @@ export default function ParticipantContainer({
   };
   const empty = Object.keys(itemList).length === 0;
   return (
-    <InnerContainer
-      className={twMerge(
-        "w-full max-h-[60vh]",
-        empty && "h-[10vh] flex items-center justify-center"
-      )}
-    >
-      {empty ? (
-        <p className="text-sm text-center text-custom-grey-text">
-          No participants added.
-        </p>
-      ) : (
-        <>
-          {admin && (
-            <div className="self-start py-3 px-3 flex items-center justify-between w-full gap-2 flex-wrap">
-              <div className="flex items-center justify-start gap-3 w-full">
-                <SecondaryButton
-                  className={twMerge(
-                    "border-custom-primary text-custom-primary min-[330px]:w-fit",
-                    selected.state && "bg-custom-primary text-custom-light-text"
-                  )}
-                  onClick={() => {
-                    setSelect({
-                      ...selected,
-                      state: !selected.state,
-                      selectedIDs: [],
-                    });
-                  }}
-                >
-                  {selected.state ? "Cancel" : "Kick Multiple"}
-                </SecondaryButton>
-                {selected.state && (
-                  <p className="text-sm text-custom-grey-text text-start">
-                    Selecting ( {selected.selectedIDs.length} )
-                  </p>
-                )}
-              </div>
-              {selected.state && (
-                <>
-                  <form
-                    onSubmit={handleSelectedFallouts}
-                    className="w-full flex items-end justify-between gap-2 max-[500px]:flex-wrap"
-                  >
-                    <FormInputContainer
-                      className="min-[500px]:flex-[2]"
-                      inputName="reason"
-                      labelText="Reason for kicking"
-                    >
-                      <input
-                        type="text"
-                        value={reason}
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                          setReason(e.target.value);
-                        }}
-                        placeholder="e.g. Guard Duty, RSI, etc."
-                        required
-                      />
-                    </FormInputContainer>
-                    <SecondaryButton
-                      type="submit"
-                      disabled={selected.selectedIDs.length === 0}
-                      className="min-[500px]:w-fit flex items-center justify-center border-custom-red text-custom-red px-3 flex-1"
-                    >
-                      {loading ? (
-                        <LoadingIcon height={20} width={20} />
-                      ) : (
-                        "Kick Selected"
-                      )}
-                    </SecondaryButton>
-                  </form>
-                  <HRow />
-                </>
+    <>
+      {admin && (
+        <div className="mb-2 flex items-center justify-between w-full gap-2 flex-wrap">
+          <div className="flex items-center justify-start gap-3 w-full">
+            <SecondaryButton
+              className={twMerge(
+                "border-custom-primary text-custom-primary min-[330px]:w-fit",
+                selected.state && "bg-custom-primary text-custom-light-text"
               )}
-            </div>
-          )}
-          {Object.keys(itemList).map((mem: string) => {
-            const tempTimestamp = itemList[mem].dateJoined;
-
-            const tempDate = new Date(tempTimestamp.seconds * 1000);
-            tempDate.setHours(tempDate.getHours() - 8);
-            const date = DateToTimestamp(tempDate);
-
-            const dateStr = TimestampToDateString(date);
-            return (
-              <div
-                key={mem}
-                onClick={() => {
-                  handleClick(mem);
-                }}
-                className={twMerge(
-                  "cursor-pointer w-full flex flex-col items-start justify-center py-2 px-3 duration-200 hover:bg-custom-light-text",
-                  selected.state && selected.selectedIDs.includes(mem)
-                    ? "bg-custom-light-orange hover:brightness-90"
-                    : "hover:bg-custom-light-text"
-                )}
+              onClick={() => {
+                setSelect({
+                  ...selected,
+                  state: !selected.state,
+                  selectedIDs: [],
+                });
+              }}
+            >
+              {selected.state ? "Cancel" : "Kick Multiple"}
+            </SecondaryButton>
+            {selected.state && (
+              <p className="text-sm text-custom-grey-text text-start">
+                Selecting ( {selected.selectedIDs.length} )
+              </p>
+            )}
+          </div>
+          {selected.state && (
+            <>
+              <form
+                onSubmit={handleSelectedFallouts}
+                className="w-full flex items-end justify-between gap-2 max-[500px]:flex-wrap"
               >
-                <h1 className="text-custom-dark-text font-semibold text-sm">
-                  {itemList[mem].displayName}
-                </h1>
-                <h4 className="text-custom-grey-text text-xs">{mem}</h4>
-                <h4 className="text-custom-grey-text text-xs">
-                  Participated on: {dateStr}
-                </h4>
-              </div>
-            );
-          })}
-        </>
+                <FormInputContainer
+                  className="min-[500px]:flex-[2]"
+                  inputName="reason"
+                  labelText="Reason for kicking"
+                >
+                  <input
+                    type="text"
+                    value={reason}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setReason(e.target.value);
+                    }}
+                    placeholder="e.g. Guard Duty, RSI, etc."
+                    required
+                  />
+                </FormInputContainer>
+                <SecondaryButton
+                  type="submit"
+                  disabled={selected.selectedIDs.length === 0}
+                  className="min-[500px]:w-fit flex items-center justify-center border-custom-red text-custom-red px-3 flex-1"
+                >
+                  {loading ? (
+                    <LoadingIcon height={20} width={20} />
+                  ) : (
+                    "Kick Selected"
+                  )}
+                </SecondaryButton>
+              </form>
+            </>
+          )}
+        </div>
       )}
-    </InnerContainer>
+      <InnerContainer
+        className={twMerge(
+          "w-full max-h-[60vh]",
+          empty && "h-[10vh] flex items-center justify-center"
+        )}
+      >
+        {empty ? (
+          <p className="text-sm text-center text-custom-grey-text">
+            No participants added.
+          </p>
+        ) : (
+          <>
+            {Object.keys(itemList).map((mem: string) => {
+              const tempTimestamp = itemList[mem].dateJoined;
+
+              const tempDate = new Date(tempTimestamp.seconds * 1000);
+              tempDate.setHours(tempDate.getHours() - 8);
+              const date = DateToTimestamp(tempDate);
+
+              const dateStr = TimestampToDateString(date);
+              return (
+                <div
+                  key={mem}
+                  onClick={() => {
+                    handleClick(mem);
+                  }}
+                  className={twMerge(
+                    "cursor-pointer w-full flex flex-col items-start justify-center py-2 px-3 duration-200 hover:bg-custom-light-text",
+                    selected.state && selected.selectedIDs.includes(mem)
+                      ? "bg-custom-light-orange hover:brightness-90"
+                      : "hover:bg-custom-light-text"
+                  )}
+                >
+                  <h1 className="text-custom-dark-text font-semibold text-sm">
+                    {itemList[mem].displayName}
+                  </h1>
+                  <h4 className="text-custom-grey-text text-xs">{mem}</h4>
+                  <h4 className="text-custom-grey-text text-xs">
+                    Participated on: {dateStr}
+                  </h4>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </InnerContainer>
+    </>
   );
 }
