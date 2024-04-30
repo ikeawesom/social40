79d25a497cca7a07d3e2536b32080b833cede253
ikeawesom <ikelimjl@gmail.com>
2024-04-30 13:46:14 +0800

    modified booked in styles

--- src/components/members/BookedStatus.tsx
+++ src/components/members/BookedStatus.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { twMerge } from "tailwind-merge";
 
 export default function BookedStatus({ status }: { status: boolean }) {
-  const bg = status ? "bg-custom-light-green" : "bg-custom-light-orange";
+  const bg = status ? "bg-custom-green" : "bg-custom-orange";
   const color = status ? "text-custom-green" : "text-custom-orange";
   const border = status ? "border-custom-green" : "border-custom-orange";
   const text = status ? "IN CAMP" : "BOOKED OUT";
@@ -10,13 +10,11 @@ export default function BookedStatus({ status }: { status: boolean }) {
   return (
     <div
       className={twMerge(
-        "px-2 py-1 border-[1px] rounded-lg",
-        bg,
-        color,
-        border
+        "h-[10px] w-[10px] rounded-full",
+        bg
+        // color,
+        // border
       )}
-    >
-      <h4 className={twMerge("text-xs", color)}>{text}</h4>
-    </div>
+    />
   );
 }
