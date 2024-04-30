08477e17f621b4383fc3411e6c482fb2839f1c8f
ikeawesom <ikelimjl@gmail.com>
2024-04-30 23:12:14 +0800

    utility badge component

---
+++ src/components/utils/Badge.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+export default function Badge({
+  children,
+  className,
+  backgroundColor,
+  primaryColor,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+  primaryColor?: string;
+  backgroundColor?: string;
+}) {
+  return (
+    <div
+      className={twMerge(
+        "px-2 py-1 border-[1px] rounded-md",
+        primaryColor ?? "border-blue-300",
+        backgroundColor ?? "bg-blue-50",
+        className
+      )}
+    >
+      <p className={twMerge("text-xs", primaryColor ?? "text-blue-300")}>
+        {children ?? "TEXT"}
+      </p>
+    </div>
+  );
+}
