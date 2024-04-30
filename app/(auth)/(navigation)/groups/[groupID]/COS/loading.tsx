bf2ff0fc711dd212be82216061bdda0a4a90dbf3
ikeawesom <ikelimjl@gmail.com>
2024-05-01 03:39:18 +0800

    added loading states

---
+++ app/(auth)/(navigation)/groups/[groupID]/COS/loading.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import DefaultSkeleton from "@/src/components/utils/DefaultSkeleton";
+import HeaderBar from "@/src/components/navigation/HeaderBar";
+
+export default function loading() {
+  return (
+    <>
+      <HeaderBar text="COS Plan for ..." back />
+      <div className="w-full grid place-items-center">
+        <div className="flex flex-col w-full items-center justify-start gap-2 max-w-[500px]">
+          <DefaultSkeleton className="h-[50px] max-w-[300px]" />
+          <DefaultSkeleton className="h-[25vh] mb-2" />
+          <DefaultSkeleton className="h-[50px] max-w-[300px]" />
+          <DefaultSkeleton className="h-[10vh]" />
+          <DefaultSkeleton className="h-[10vh]" />
+          <DefaultSkeleton className="h-[10vh]" />
+        </div>
+      </div>
+    </>
+  );
+}
