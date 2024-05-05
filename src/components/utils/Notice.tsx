c7a70456c3e9ba8d3ac4f5a85ec45017778b9b07
ikeawesom <ikelimjl@gmail.com>
2024-05-05 21:21:07 +0800

    removed closed property

--- src/components/utils/Notice.tsx
+++ src/components/utils/Notice.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
-import Image from "next/image";
 
 type NoticeType = {
   status: "info" | "success" | "warning" | "error";
@@ -8,7 +7,6 @@ type NoticeType = {
   containerClassName?: string;
   textClassName?: string;
   noHeader?: boolean;
-  close?: () => void;
 };
 export default function Notice({
   status,
@@ -16,21 +14,20 @@ export default function Notice({
   containerClassName,
   textClassName,
   noHeader,
-  close,
 }: NoticeType) {
   return (
     <div
       className={twMerge(
-        "w-full rounded-lg border-[1.2px] p-2 relative",
+        "w-full rounded-lg border-[1.2px] p-2 px-3 relative",
         status === "info" && "bg-blue-50 border-blue-700/30",
         status === "success" && "bg-custom-light-green border-custom-green/30",
         status === "error" && "bg-custom-light-red border-custom-red/30",
         status === "warning" &&
-          "bg-custom-light-orange border-custom-orange/30",
+          "bg-custom-light-orange/50 border-custom-orange/30",
         containerClassName
       )}
     >
-      <p
+      <div
         className={twMerge(
           status === "info" && "text-blue-700",
           status === "success" && "text-custom-green",
@@ -44,17 +41,7 @@ export default function Notice({
           <span className="font-semibold">[{status.toUpperCase()}]</span>
         )}{" "}
         {children ?? "This is a notice card."}
-      </p>
-      {close && (
-        <Image
-          src="/icons/icon_close.svg"
-          alt="Close"
-          width={10}
-          height={10}
-          onClick={close}
-          className="absolute top-3 right-3 cursor-pointer hover:opacity-70"
-        />
-      )}
+      </div>
     </div>
   );
 }
