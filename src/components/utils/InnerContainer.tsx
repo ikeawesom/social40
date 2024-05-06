80d3b24fb2c4866b008dd73ffdab8735a9c3bc15
ikeawesom <ikelimjl@gmail.com>
2024-05-07 07:11:02 +0800

    modified styles

--- src/components/utils/InnerContainer.tsx
+++ src/components/utils/InnerContainer.tsx
@@ -4,14 +4,17 @@ import { twMerge } from "tailwind-merge";
 export default function InnerContainer({
   children,
   className,
+  noBorder,
 }: {
   children?: React.ReactNode;
   className?: string;
+  noBorder?: boolean;
 }) {
   return (
     <div
       className={twMerge(
         "relative w-full flex-col flex items-center justify-start overflow-y-scroll rounded-lg shadow-inner",
+        !noBorder && "border-[1px] border-custom-light-text",
         className
       )}
     >
