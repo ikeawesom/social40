9f2c7aa712f543ee419dac3385518498406354c4
ikeawesom <ikelimjl@gmail.com>
2024-05-06 15:48:43 +0800

    server functions to handle group strengths and statuses

---
+++ src/components/groups/custom/strength/handleGroupStatuses.ts
@@ -0,0 +1,40 @@
+import { dbHandler } from "@/src/firebase/db";
+import handleResponses from "@/src/utils/handleResponses";
+import { GroupStatusType } from "@/src/utils/schemas/groups";
+
+export async function handleGroupStatuses(memberList: string[]) {
+  // fetch member's statuses
+  try {
+    const statusArr = memberList.map(async (memberID: string) => {
+      const { data, error } = await dbHandler.getSpecific({
+        path: `MEMBERS/${memberID}/STATUSES`,
+        orderCol: "endDate",
+        ascending: false,
+      });
+
+      if (error) return handleResponses({ status: false, error });
+
+      const statusData = {
+        [memberID]: data,
+      };
+
+      return handleResponses({ data: statusData });
+    });
+
+    const statusPromiseArray = await Promise.all(statusArr);
+
+    const groupStatusList = {} as GroupStatusType;
+
+    statusPromiseArray.forEach((item: any) => {
+      if (!item.status) throw new Error(item.error);
+      const data = item.data;
+      const memberID = Object.keys(data)[0];
+      const memberStatusObj = data[memberID];
+      groupStatusList[memberID] = memberStatusObj;
+    });
+
+    return handleResponses({ data: groupStatusList });
+  } catch (err: any) {
+    return handleResponses({ status: false, error: err.message });
+  }
+}
---
+++ src/components/groups/custom/strength/handleGroupStrength.ts
@@ -0,0 +1,44 @@
+import { dbHandler } from "@/src/firebase/db";
+import { GROUP_ROLES_HEIRARCHY } from "@/src/utils/constants";
+import { GroupDetailsType } from "@/src/utils/schemas/groups";
+
+export async function getUpdatedMembers(membersList: GroupDetailsType) {
+  const updatedMembers = {} as GroupDetailsType;
+  const promiseArr = Object.keys(membersList).map(async (id: string) => {
+    const { data, error } = await dbHandler.get({
+      col_name: "MEMBERS",
+      id,
+    });
+
+    if (error) return { id: null, bookedIn: null, pfp: null };
+    return { id: data.memberID, bookedIn: data.bookedIn, pfp: data.pfp ?? "" };
+  });
+
+  const resolvedArr = await Promise.all(promiseArr);
+
+  resolvedArr.forEach((item: any) => {
+    updatedMembers[item.id] = {
+      ...membersList[item.id],
+      bookedIn: item.bookedIn,
+      pfp: item.pfp,
+    };
+  });
+
+  return updatedMembers;
+}
+
+export function sortMembersList(membersList: GroupDetailsType) {
+  const temp = {} as GroupDetailsType;
+
+  const sortedId = Object.keys(membersList).sort(
+    (a, b) =>
+      GROUP_ROLES_HEIRARCHY[membersList[b].role].rank -
+      GROUP_ROLES_HEIRARCHY[membersList[a].role].rank
+  );
+
+  sortedId.forEach((id: string) => {
+    temp[id] = membersList[id];
+  });
+
+  return temp;
+}
