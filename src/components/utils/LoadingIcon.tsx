182a174940a4bee37c0efe709bdf482313a2c224
ikeawesom <ikelimjl@gmail.com>
2024-05-06 23:05:38 +0800

    added style

--- src/components/utils/LoadingIcon.tsx
+++ src/components/utils/LoadingIcon.tsx
@@ -1,31 +1,33 @@
 import React from "react";
 import Image from "next/image";
+import { twMerge } from "tailwind-merge";
 
 type LoadingType = {
   width?: number;
   height?: number;
+  className?: string;
 };
 
-export default function LoadingIcon({ height, width }: LoadingType) {
+export default function LoadingIcon({ height, width, className }: LoadingType) {
   return (
     <Image
       src="/icons/icon_spinner.svg"
       width={height ? height : 100}
       height={width ? width : 100}
       alt="Loading..."
-      className="animate-spin"
+      className={twMerge("animate-spin", className)}
     />
   );
 }
 
-export function LoadingIconBright({ height, width }: LoadingType) {
+export function LoadingIconBright({ height, width, className }: LoadingType) {
   return (
     <Image
       src="/icons/icon_spinner_bright.svg"
       width={height ? height : 100}
       height={width ? width : 100}
       alt="Loading..."
-      className="animate-spin"
+      className={twMerge("animate-spin", className)}
     />
   );
 }
