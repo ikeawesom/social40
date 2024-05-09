25e20862008fb5b547aa0c96029e746dea07ae63
ikeawesom <ikelimjl@gmail.com>
2024-05-10 00:23:53 +0800

    fixed undefined bug

--- src/components/groups/custom/leaderboard/GroupLeaderboardClient.tsx
+++ src/components/groups/custom/leaderboard/GroupLeaderboardClient.tsx
@@ -68,79 +68,79 @@ export default function GroupLeaderboardClient({
 
   //     sortedTop = temp;
   //   }
-  let filtered = [] as string[];
-  if (top) {
+
+  if (top !== undefined) {
+    let filtered = [] as string[];
     filtered = Object.keys(top).splice(0, 3);
-  }
+    const curMemberPoints = top[curMember] ? top[curMember].points ?? 0 : 0;
 
-  const curMemberPoints = top ? top[curMember].points ?? 0 : 0;
-
-  return (
-    <div className="w-full flex items-start justify-start flex-col gap-0 mt-2">
-      <div className="w-full flex items-center justify-center rounded-md overflow-hidden shadow-sm border-[1px] border-custom-light-text">
-        {ScoreCat.map((type: string, id: number) => (
-          <SecondaryButton
-            onClick={() => setCat(type)}
-            className="rounded-none shadow-none border-none px-3 border-l-[1px]"
-            activatedStyles="bg-custom-primary text-custom-light-text font-bold"
-            activated={type === cat}
-            key={id}
-          >
-            {type}
-          </SecondaryButton>
-        ))}
-      </div>
-
-      {top !== undefined && filtered.length > 0 ? (
-        <>
-          <div className="w-full flex items-start justify-start gap-2 flex-col mt-4">
-            <LeaderboardTab
-              curMember={curMember}
-              type={"GOLD"}
-              member={top[filtered[0]]}
-            />
-            <LeaderboardTab
-              curMember={curMember}
-              type={"SILVER"}
-              member={top[filtered[1]]}
-            />
-            <LeaderboardTab
-              curMember={curMember}
-              type={"BRONZE"}
-              member={top[filtered[2]]}
-            />
-          </div>
-          {!filtered.includes(curMember) &&
-            ((admin && cat !== "MEMBERS") || (!admin && cat === "MEMBERS")) &&
-            curMemberPoints > 0 && (
-              <div className="w-full flex-col items-center justify-center">
-                <p className="text-center text-custom-grey-text">:</p>
-                <LeaderboardTab
-                  type="DEFAULT"
-                  className="shadow-sm border-[1px] border-custom-light-text/50 hover:bg-custom-light-text/70"
-                  curMember={curMember}
-                  member={top[curMember]}
-                />
-              </div>
-            )}
-        </>
-      ) : filtered.length === 0 ? (
-        <ErrorSection>
-          Oops, looks like nobody here is qualified on the leaderboard!
-        </ErrorSection>
-      ) : (
-        <div className="flex flex-col w-full items-start justify-start gap-3 mt-4">
-          <LeaderboardTabSkeleton isBest />
-          <LeaderboardTabSkeleton />
-          <LeaderboardTabSkeleton />
+    return (
+      <div className="w-full flex items-start justify-start flex-col gap-0 mt-2">
+        <div className="w-full flex items-center justify-center rounded-md overflow-hidden shadow-sm border-[1px] border-custom-light-text">
+          {ScoreCat.map((type: string, id: number) => (
+            <SecondaryButton
+              onClick={() => setCat(type)}
+              className="rounded-none shadow-none border-none px-3 border-l-[1px]"
+              activatedStyles="bg-custom-primary text-custom-light-text font-bold"
+              activated={type === cat}
+              key={id}
+            >
+              {type}
+            </SecondaryButton>
+          ))}
         </div>
-      )}
-      <Link
-        className="text-start underline text-sm duration-150 text-custom-grey-text hover:text-custom-primary mt-3"
-        href={`/groups/${groupID}/leaderboard`}
-      >
-        View all leaderboards
-      </Link>
-    </div>
-  );
+
+        {top !== undefined && filtered.length > 0 ? (
+          <>
+            <div className="w-full flex items-start justify-start gap-2 flex-col mt-4">
+              <LeaderboardTab
+                curMember={curMember}
+                type={"GOLD"}
+                member={top[filtered[0]]}
+              />
+              <LeaderboardTab
+                curMember={curMember}
+                type={"SILVER"}
+                member={top[filtered[1]]}
+              />
+              <LeaderboardTab
+                curMember={curMember}
+                type={"BRONZE"}
+                member={top[filtered[2]]}
+              />
+            </div>
+            {!filtered.includes(curMember) &&
+              ((admin && cat !== "MEMBERS") || (!admin && cat === "MEMBERS")) &&
+              curMemberPoints > 0 && (
+                <div className="w-full flex-col items-center justify-center">
+                  <p className="text-center text-custom-grey-text">:</p>
+                  <LeaderboardTab
+                    type="DEFAULT"
+                    className="shadow-sm border-[1px] border-custom-light-text/50 hover:bg-custom-light-text/70"
+                    curMember={curMember}
+                    member={top[curMember]}
+                  />
+                </div>
+              )}
+          </>
+        ) : filtered.length === 0 ? (
+          <ErrorSection>
+            Oops, looks like nobody here is qualified on the leaderboard!
+          </ErrorSection>
+        ) : (
+          <div className="flex flex-col w-full items-start justify-start gap-3 mt-4">
+            <LeaderboardTabSkeleton isBest />
+            <LeaderboardTabSkeleton />
+            <LeaderboardTabSkeleton />
+          </div>
+        )}
+        <Link
+          className="text-start underline text-sm duration-150 text-custom-grey-text hover:text-custom-primary mt-3"
+          href={`/groups/${groupID}/leaderboard`}
+        >
+          View all leaderboards
+        </Link>
+      </div>
+    );
+  }
 }
