ee9b9f000d35d6b4f3d86aa59f68f2beb7172a5d
ikeawesom <ikelimjl@gmail.com>
2024-05-15 02:02:08 +0800

    client components for pagination

---
+++ src/components/feed/FeedGroupCardClient.tsx
@@ -0,0 +1,192 @@
+"use client";
+
+import { FetchGroupActivityData } from "@/src/utils/activities/group/FetchData";
+import {
+  GROUP_ACTIVITY_PARTICIPANT,
+  GROUP_ACTIVITY_SCHEMA,
+} from "@/src/utils/schemas/group-activities";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+import DefaultCard from "../DefaultCard";
+import JoinGroupActivityButton from "../groups/custom/activities/settings/JoinGroupActivityButton";
+import HRow from "../utils/HRow";
+import SecondaryButton from "../utils/SecondaryButton";
+import ActivityStatusTab from "./ActivityStatusTab";
+import DismissButton from "./DismissButton";
+import Image from "next/image";
+import { useHostname } from "@/src/hooks/useHostname";
+import FeedGroupCardSkeleton from "./FeedGroupCardSkeleton";
+
+export default function FeedGroupCardClient({
+  activityData,
+  memberID,
+}: {
+  activityData: GROUP_ACTIVITY_SCHEMA;
+  memberID: string;
+}) {
+  const [updatedData, setUpdatedData] = useState<{
+    canJoin: boolean;
+    active: boolean;
+    dateStr: string;
+    currentParticipant: boolean;
+    participantsData: {
+      [memberID: string]: GROUP_ACTIVITY_PARTICIPANT;
+    };
+    requested: boolean;
+  }>();
+
+  const { activityID, groupID, activityDesc, activityTitle, isPT } =
+    activityData;
+
+  const { host } = useHostname();
+
+  const fetchData = async () => {
+    try {
+      const res = await FetchGroupActivityData.getMain({
+        activityID,
+        groupID,
+        host,
+        memberID,
+      });
+
+      if (!res.status) throw new Error(res.error);
+
+      const { canJoin, active, dateStr, currentParticipant, participantsData } =
+        res.data;
+
+      const resA = await FetchGroupActivityData.getRequests({
+        activityID,
+        groupID,
+        host,
+        memberID,
+      });
+
+      if (!resA.status) throw new Error(resA.error);
+
+      const { requested } = resA.data;
+
+      setUpdatedData({
+        active,
+        dateStr,
+        currentParticipant,
+        participantsData,
+        canJoin,
+        requested,
+      });
+    } catch (err: any) {
+      toast.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!updatedData) return <FeedGroupCardSkeleton />;
+
+  const {
+    requested,
+    active,
+    canJoin,
+    currentParticipant,
+    dateStr,
+    participantsData,
+  } = updatedData;
+
+  const participantNumber = Object.keys(participantsData).length;
+  // const randomIndex = Math.floor(Math.random() * participantNumber);
+
+  const randomParticipant = Object.keys(participantsData)[0];
+  const oneParticipant = participantNumber === 1;
+  const noParticipant = participantNumber === 0;
+
+  return (
+    <DefaultCard className="w-full flex flex-col items-start justify-start">
+      <Link
+        href={`/groups/${groupID}`}
+        className="text-xs text-custom-grey-text duration-150 hover:opacity-70"
+      >
+        {groupID}
+      </Link>
+      <HRow className="mb-2" />
+      <div className="w-full flex items-center justify-between flex-wrap gap-x-2">
+        <Link
+          href={`/groups/${activityData.groupID}/activity?${new URLSearchParams(
+            {
+              id: activityID,
+            }
+          )}`}
+          className="text-start font-semibold text-lg text-custom-dark-text flex items-start justify-start gap-2 hover:opacity-70"
+        >
+          {activityTitle}
+          {isPT && (
+            <Image
+              alt="PT activity"
+              className="my-1"
+              src="/icons/features/icon_activities_active.svg"
+              width={20}
+              height={20}
+            />
+          )}
+        </Link>
+        <ActivityStatusTab active={active} />
+      </div>
+      <p className="text-custom-dark-text text-sm">{activityDesc}</p>
+      <p className="text-custom-grey-text text-xs">
+        {active ? "Begins on: " : "Ended on: "}
+        {dateStr}
+      </p>
+
+      <Link
+        href={`/groups/${activityData.groupID}/activity?${new URLSearchParams({
+          id: activityID,
+        })}`}
+        className="text-sm text-custom-dark-text mt-2"
+      >
+        {!noParticipant && (
+          <>
+            <span className="font-semibold">{randomParticipant} </span>
+            {oneParticipant ? (
+              "is "
+            ) : (
+              <span>
+                `and{" "}
+                <span className="font-semibold">
+                  {participantNumber - 1} others
+                </span>{" "}
+                are{" "}
+              </span>
+            )}
+            participating.
+          </>
+        )}
+      </Link>
+
+      <div className="w-full mt-2 flex items-center justify-end gap-3 max-[350px]:flex-wrap">
+        {!currentParticipant ? (
+          <JoinGroupActivityButton
+            activityID={activityID}
+            canJoin={canJoin}
+            memberID={memberID}
+            requested={requested}
+            className="text-xs"
+          />
+        ) : (
+          <SecondaryButton
+            disabled
+            className="border-custom-green text-custom-green text-xs px-3 min-w-fit"
+          >
+            {active ? "Participating" : "Participated"}
+          </SecondaryButton>
+        )}
+
+        <DismissButton
+          activityID={activityID}
+          host={host}
+          memberID={memberID}
+        />
+      </div>
+    </DefaultCard>
+  );
+}
---
+++ src/components/feed/FeedGroupClient.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { FetchPaginateActivity } from "@/src/utils/home/ActivityFeed";
+import { GROUP_ACTIVITY_SCHEMA } from "@/src/utils/schemas/group-activities";
+import React, { useEffect, useState } from "react";
+import { toast } from "sonner";
+import FeedGroupCardClient from "./FeedGroupCardClient";
+import { useMemberID } from "@/src/hooks/useMemberID";
+import ActivityFeedSkeleton from "./ActivityFeedSkeleton";
+import LoadingIcon from "../utils/LoadingIcon";
+import { twMerge } from "tailwind-merge";
+
+export type PaginatedActivityData = {
+  activities: {
+    [id: string]: GROUP_ACTIVITY_SCHEMA;
+  };
+  pointer: any;
+};
+
+export default function FeedGroupClient({
+  hidden,
+  groupID,
+}: {
+  hidden: string[];
+  groupID: string;
+}) {
+  const [loading, setLoading] = useState(false);
+  const { memberID } = useMemberID();
+  const [activityData, setActivityData] = useState<PaginatedActivityData>({
+    activities: {},
+    pointer: null,
+  });
+  const [fetch, setFetch] = useState(0);
+  const [finished, setFinished] = useState(false);
+
+  const fetchData = async () => {
+    if (finished) return;
+
+    try {
+      const { data: pagiData, error } = await FetchPaginateActivity({
+        groupID,
+        lastPointer: activityData.pointer ?? null,
+      });
+
+      if (error) throw new Error(error);
+      const { data, lastPointer } = pagiData;
+
+      hidden.forEach((id: string) => {
+        delete data[id];
+      });
+
+      setActivityData({
+        pointer: lastPointer,
+        activities: { ...activityData.activities, ...data },
+      });
+    } catch (err: any) {
+      const { message } = err;
+      if (message.includes("undefined")) {
+        // finished all data
+        setFinished(true);
+      } else {
+        toast.error(err.message);
+      }
+    }
+    setLoading(false);
+  };
+
+  const handleScroll = async () => {
+    if (finished) return;
+    // console.log("here");
+    // console.log("H:", document.documentElement.scrollHeight);
+    // console.log("T:", document.documentElement.scrollTop);
+    // console.log("W:", window.innerHeight);
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >=
+      document.documentElement.scrollHeight
+    ) {
+      setLoading(true);
+      setTimeout(() => {
+        setFetch((fetch) => fetch + 1);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!finished) fetchData();
+    if (finished) setLoading(false);
+  }, [fetch]);
+
+  if (Object.keys(activityData.activities).length === 0)
+    return <ActivityFeedSkeleton />;
+
+  return (
+    <div
+      className={twMerge(
+        "flex w-full flex-col items-center justify-center gap-4",
+        !finished && "pb-6"
+      )}
+    >
+      {Object.keys(activityData.activities).map((id: string) => {
+        return (
+          <FeedGroupCardClient
+            activityData={JSON.parse(
+              JSON.stringify(activityData.activities[id])
+            )}
+            memberID={memberID}
+            key={id}
+          />
+        );
+      })}
+      {loading && !finished && (
+        <div className="grid place-items-center w-full">
+          <LoadingIcon height={40} width={40} />
+        </div>
+      )}
+    </div>
+  );
+}
