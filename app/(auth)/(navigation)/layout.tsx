d4e000e37ff9b6aea134da1214ffa690cbe0e508
ikeawesom <ikelimjl@gmail.com>
2024-05-15 18:44:15 +0800

    added debugging condition

--- app/(auth)/(navigation)/layout.tsx
+++ app/(auth)/(navigation)/layout.tsx
@@ -1,6 +1,16 @@
 import Navbar from "@/src/components/navigation/Navbar";
+import MaintenanceScreen from "@/src/components/screens/MaintenanceScreen";
+import SignInAgainScreen from "@/src/components/screens/SignInAgainScreen";
+import { IS_DEBUG } from "@/src/utils/settings";
+import { cookies } from "next/headers";
 
 export default function NavLayout({ children }: { children: React.ReactNode }) {
+  const cookieStore = cookies();
+  const data = cookieStore.get("memberID");
+
+  if (!data) return <SignInAgainScreen />;
+
+  if (IS_DEBUG) return <MaintenanceScreen />;
   return (
     <>
       <div className="mb-16 mt-10">{children}</div>
