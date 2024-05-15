2f5f3b87ee9f402a10073bb72b25ae7450f6bec8
ikeawesom <ikelimjl@gmail.com>
2024-05-15 23:48:02 +0800

    filtered within fetch function

--- src/utils/home/ActivityFeed.ts
+++ src/utils/home/ActivityFeed.ts
@@ -2,13 +2,16 @@
 
 import { dbHandler } from "@/src/firebase/db";
 import handleResponses from "../handleResponses";
+import { GROUP_ACTIVITIES_SCHEMA } from "../schemas/groups";
 
 export async function FetchPaginateActivity({
   groupID,
   lastPointer,
+  hidden,
 }: {
   groupID: string;
-  lastPointer: any;
+  hidden: string[];
+  lastPointer?: any;
 }) {
   try {
     let pointerRef = null;
@@ -33,9 +36,15 @@ export async function FetchPaginateActivity({
 
     const { data: limitedData, lastPointer: pointer } = paginateRes;
 
+    // filter hidden activity IDs from all activities keys
+    const actArr = [] as GROUP_ACTIVITIES_SCHEMA[];
+    Object.keys(limitedData).forEach((id: string) => {
+      if (!hidden.includes(id)) actArr.push(limitedData[id]);
+    });
+
     return handleResponses({
       data: {
-        data: JSON.parse(JSON.stringify(limitedData)),
+        data: JSON.parse(JSON.stringify(actArr)),
         lastPointer: pointer,
       },
     });
