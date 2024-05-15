a6c43edc31f9295315422cfceb07bb114a359436
ikeawesom <ikelimjl@gmail.com>
2024-05-15 18:52:39 +0800

    added condition for maintennance screen

--- app/(auth)/(navigation)/layout.tsx
+++ app/(auth)/(navigation)/layout.tsx
@@ -1,16 +1,36 @@
 import Navbar from "@/src/components/navigation/Navbar";
 import MaintenanceScreen from "@/src/components/screens/MaintenanceScreen";
 import SignInAgainScreen from "@/src/components/screens/SignInAgainScreen";
+import { dbHandler } from "@/src/firebase/db";
+import { ROLES_HIERARCHY } from "@/src/utils/constants";
+import { MEMBER_SCHEMA } from "@/src/utils/schemas/members";
 import { IS_DEBUG } from "@/src/utils/settings";
 import { cookies } from "next/headers";
 
-export default function NavLayout({ children }: { children: React.ReactNode }) {
+export default async function NavLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const cookieStore = cookies();
-  const data = cookieStore.get("memberID");
+  const cookieItem = cookieStore.get("memberID");
 
-  if (!data) return <SignInAgainScreen />;
+  if (!cookieItem) return <SignInAgainScreen />;
 
-  if (IS_DEBUG) return <MaintenanceScreen />;
+  if (IS_DEBUG.status) {
+    if (IS_DEBUG.membersOnly) {
+      const id = cookieItem.value;
+      const { data }: { data: MEMBER_SCHEMA } = await dbHandler.get({
+        col_name: "MEMBERS",
+        id,
+      });
+      const { role } = data;
+      if (ROLES_HIERARCHY[role].rank < ROLES_HIERARCHY["admin"].rank)
+        return <MaintenanceScreen />;
+    } else {
+      return <MaintenanceScreen />;
+    }
+  }
   return (
     <>
       <div className="mb-16 mt-10">{children}</div>
