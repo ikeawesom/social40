5997af7e9ae90afba6632c1be7917da07535c0a2
ikeawesom <ikelimjl@gmail.com>
2024-05-18 15:09:06 +0800

    added new settings for maintenance

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,4 +1,8 @@
-export const IS_DEBUG = { status: true, membersOnly: true };
+export const IS_DEBUG = { status: true, membersOnly: false };
+export const isFullMaintenance =
+  IS_DEBUG.status &&
+  !IS_DEBUG.membersOnly &&
+  process.env.NODE_ENV === "production";
 
 export const MAX_LENGTH = 30;
 
