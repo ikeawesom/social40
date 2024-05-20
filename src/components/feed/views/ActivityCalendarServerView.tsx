c82d48d99ef243d5f27774c4a768a60f382be26e
ikeawesom <ikelimjl@gmail.com>
2024-05-21 00:18:52 +0800

    cleaned up

--- src/components/feed/views/ActivityCalendarServerView.tsx
+++ src/components/feed/views/ActivityCalendarServerView.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import ErrorScreenHandler from "../../ErrorScreenHandler";
-import { dbHandler } from "@/src/firebase/db";
 import {
   getMonthStartAndEnd,
   getWeekStartAndEnd,
 } from "@/src/utils/helpers/getCurrentDate";
-import { getSimple } from "@/src/utils/helpers/parser";
-import ActivityCalendarClientView, {
-  FullActivityType,
-} from "./ActivityCalendarClientView";
+import ActivityCalendarClientView from "./ActivityCalendarClientView";
 
 export default async function ActivityCalendarServerView({
   groupID,
@@ -26,26 +22,6 @@ export default async function ActivityCalendarServerView({
       view === "weekly"
         ? getWeekStartAndEnd(curDate)
         : getMonthStartAndEnd(curDate);
-    console.log("1.", startDate);
-    // // startDate.setHours(startDate.getHours() + 8);
-    // const { data, error } = await dbHandler.getSpecific({
-    //   path: `GROUP-ACTIVITIES`,
-    //   orderCol: "activityDate",
-    //   ascending: false,
-    //   field: "activityDate",
-    //   criteria: "<=",
-    //   value: endDate,
-    //   field2: "activityDate",
-    //   criteria2: ">=",
-    //   value2: startDate,
-    //   field3: "groupID",
-    //   criteria3: "in",
-    //   value3: all ?? [groupID],
-    // });
-
-    // if (error) throw new Error(error);
-
-    // const parsed = getSimple(data) as FullActivityType;
 
     return (
       <ActivityCalendarClientView
