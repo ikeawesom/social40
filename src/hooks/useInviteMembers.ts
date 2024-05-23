70ee01670cc8fb97b4de6ecefc6fd4bf91410e90
ikeawesom <ikelimjl@gmail.com>
2024-05-24 02:03:21 +0800

    custom hook for invitiation form

---
+++ src/hooks/useInviteMembers.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useHostname } from "./useHostname";
+import { useRouter } from "next/navigation";
+
+export function useInviteMembers() {
+  const router = useRouter();
+  const { host } = useHostname();
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
+  const [members, setMembers] = useState<string[]>([]);
+  return {
+    router,
+    host,
+    loading,
+    setLoading,
+    errors,
+    setErrors,
+    members,
+    setMembers,
+  };
+}
