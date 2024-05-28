cc01c06ad271becaab802b84d224acfd7c020c45
ikeawesom <ikelimjl@gmail.com>
2024-05-28 20:47:18 +0800

    using loading icon

--- src/components/announcements/card/DeleteButton.tsx
+++ src/components/announcements/card/DeleteButton.tsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import { deletePost } from "../submitPostData";
 import { toast } from "sonner";
 import Image from "next/image";
+import LoadingIcon from "../../utils/LoadingIcon";
 
 export default function DeleteButton({
   id,
@@ -11,15 +12,16 @@ export default function DeleteButton({
   id: string;
   haveMedia: boolean;
 }) {
-  const [loading, setLoading] = useState("");
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
+
   const handleDelete = async () => {
     if (
       confirm(
         "Are you sure you want to delete this post? This is irreversible!"
       )
     ) {
-      setLoading("Deleting...");
+      setLoading(true);
       try {
         const res = await deletePost(id ?? "", haveMedia);
         if (!res.status) throw new Error(res.error);
@@ -27,13 +29,14 @@ export default function DeleteButton({
         // toast.success("Post deleted");
       } catch (err: any) {
         toast.error(err.message);
-        setLoading("");
+        setLoading(false);
       }
     }
   };
+
   return (
     <>
-      {loading === "" ? (
+      {!loading ? (
         <Image
           onClick={handleDelete}
           className="cursor-pointer self-end"
@@ -43,7 +46,7 @@ export default function DeleteButton({
           width={25}
         />
       ) : (
-        <p className="self-end text-sm text-custom-grey-text">{loading}</p>
+        <LoadingIcon width={20} height={20} />
       )}
     </>
   );
