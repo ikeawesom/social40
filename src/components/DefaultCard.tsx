a588ff6dff6cad45e06cdde465a247523213c2f9
ikeawesom <ikelimjl@gmail.com>
2024-05-30 16:56:01 +0800

    modified props

--- src/components/DefaultCard.tsx
+++ src/components/DefaultCard.tsx
@@ -4,10 +4,18 @@ import { twMerge } from "tailwind-merge";
 type CardType = {
   children?: React.ReactNode;
   className?: string;
+  onClick?: () => void;
 };
-export default function DefaultCard({ children, className }: CardType) {
+export default function DefaultCard({
+  children,
+  className,
+  onClick,
+}: CardType) {
   return (
-    <div className={twMerge("bg-white shadow-sm p-4 rounded-lg", className)}>
+    <div
+      onClick={onClick}
+      className={twMerge("bg-white shadow-sm p-4 rounded-lg", className)}
+    >
       {children}
     </div>
   );
--- src/components/profile/ProfileStatSection.tsx
+++ src/components/profile/ProfileStatSection.tsx
@@ -5,19 +5,21 @@ import { twMerge } from "tailwind-merge";
 export interface ProfileStatSectionType extends ProfileStatType {
   className?: string;
   title?: string;
+  onClick?: () => void;
 }
 export default function ProfileStatSection({
   config,
   title,
   value,
   className,
+  onClick,
 }: ProfileStatSectionType) {
   return (
     <div className={twMerge("w-full", className)}>
       <p className="text-xs text-custom-dark-text mb-1 font-bold">
         {title ?? "Title"}
       </p>
-      <ProfileStatCard config={config} value={value} />
+      <ProfileStatCard onClick={onClick} config={config} value={value} />
     </div>
   );
 }
