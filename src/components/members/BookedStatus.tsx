c5fc220bf6070c5840332521a4528096ad78391c
ikeawesom <ikelimjl@gmail.com>
2024-06-10 16:03:05 +0800

    using client state bibo context

--- src/components/members/BookedStatus.tsx
+++ src/components/members/BookedStatus.tsx
@@ -1,20 +1,27 @@
-import React from "react";
+"use client";
+
+import { useBiboCtx } from "@/src/contexts/BiboContext";
+import React, { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 
 export default function BookedStatus({ status }: { status: boolean }) {
-  const bg = status ? "bg-custom-green" : "bg-custom-orange";
-  const color = status ? "text-custom-green" : "text-custom-orange";
-  const border = status ? "border-custom-green" : "border-custom-orange";
-  const text = status ? "IN CAMP" : "BOOKED OUT";
+  const { clientBibo, setClientBibo } = useBiboCtx();
 
-  return (
-    <div
-      className={twMerge(
-        "h-[10px] w-[10px] rounded-full",
-        bg
-        // color,
-        // border
-      )}
-    />
-  );
+  const bg = clientBibo ? "bg-custom-green" : "bg-custom-orange";
+
+  useEffect(() => {
+    if (clientBibo === null) setClientBibo(status);
+  }, []);
+
+  if (clientBibo !== null)
+    return (
+      <div
+        className={twMerge(
+          "h-[10px] w-[10px] rounded-full",
+          bg
+          // color,
+          // border
+        )}
+      />
+    );
 }
