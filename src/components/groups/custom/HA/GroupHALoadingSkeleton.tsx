dfdb7ddb5ad4eb8c3cd5497c58c28d31056c2df8
ikeawesom <ikelimjl@gmail.com>
2024-06-18 19:44:02 +0800

    modify loading screen

--- src/components/groups/custom/HA/GroupHALoadingSkeleton.tsx
+++ src/components/groups/custom/HA/GroupHALoadingSkeleton.tsx
@@ -1,19 +1,11 @@
-import DefaultSkeleton from "@/src/components/utils/DefaultSkeleton";
+import CenterFeedSkeleton from "@/src/components/utils/CenterFeedSkeleton";
 import React from "react";
 
 export default function GroupHALoadingSkeleton() {
   return (
     <div className="w-full grid place-items-center">
       <div className="flex flex-col w-full items-center justify-start gap-2 max-w-[500px]">
-        <div className="flex flex-col items-center justify-center gap-1">
-          <DefaultSkeleton className="h-[20px] w-[200px]" />
-          <DefaultSkeleton className="h-[5px] w-[100px]" />
-          <DefaultSkeleton className="h-[8px] w-[100px] mt-3" />
-        </div>
-        <DefaultSkeleton className="h-[7vh]" />
-        <DefaultSkeleton className="h-[7vh]" />
-        <DefaultSkeleton className="h-[7vh]" />
-        <DefaultSkeleton className="h-[7vh]" />
+        <CenterFeedSkeleton height="h-[8vh]" />
       </div>
     </div>
   );
