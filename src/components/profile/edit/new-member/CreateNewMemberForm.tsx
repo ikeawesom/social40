35cfee94790b2559329c0eed609e24810349372b
ikeawesom <ikelimjl@gmail.com>
2024-06-18 23:53:15 +0800

    added permission modal

--- src/components/profile/edit/new-member/CreateNewMemberForm.tsx
+++ src/components/profile/edit/new-member/CreateNewMemberForm.tsx
@@ -1,16 +1,17 @@
 "use client";
-import { ROLES_HIERARCHY } from "@/src/utils/constants";
+import { ROLES_DESC, ROLES_HIERARCHY } from "@/src/utils/constants";
 import React, { useState } from "react";
 import SecondaryButton from "../../../utils/SecondaryButton";
 import Image from "next/image";
 import { MEMBER_SCHEMA } from "@/src/utils/schemas/members";
-import HRow from "../../../utils/HRow";
 import PrimaryButton from "../../../utils/PrimaryButton";
 import { toast } from "sonner";
 import { useHostname } from "@/src/hooks/useHostname";
 import { Onboarding } from "@/src/utils/onboarding";
 import { GetPostObj } from "@/src/utils/API/GetPostObj";
 import { LoadingIconBright } from "../../../utils/LoadingIcon";
+import Modal from "@/src/components/utils/Modal";
+import ModalHeader from "@/src/components/utils/ModalHeader";
 
 type NewMemberType = {
   memberData: MEMBER_SCHEMA;
@@ -25,8 +26,9 @@ export default function CreateNewMemberForm({ memberData }: NewMemberType) {
   });
 
   const [newRole, setNewRole] = useState("member");
-  const [showRoles, setShowRoles] = useState(false);
+  const [showRoles, setShowRoles] = useState("");
   const [show, setShow] = useState(false);
+
   const [loading, setLoading] = useState(false);
 
   const email = `${newDetails.id}@digital40sar.com`;
@@ -39,7 +41,7 @@ export default function CreateNewMemberForm({ memberData }: NewMemberType) {
   const getPermissions = (roleA: string) => {
     var obj = {} as any;
     Object.keys(ROLES_HIERARCHY).forEach((item: any) => {
-      if (ROLES_HIERARCHY[item].rank < ROLES_HIERARCHY[roleA].rank) {
+      if (ROLES_HIERARCHY[item].rank <= ROLES_HIERARCHY[roleA].rank) {
         obj[item] = ROLES_HIERARCHY[item];
       }
     });
@@ -47,6 +49,8 @@ export default function CreateNewMemberForm({ memberData }: NewMemberType) {
   };
 
   const permissionList = getPermissions(currentRole);
+  const defaultPerms =
+    Object.keys(permissionList)[Object.keys(permissionList).length - 1];
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNewDetails({ ...newDetails, [e.target.name]: e.target.value });
@@ -93,79 +97,114 @@ export default function CreateNewMemberForm({ memberData }: NewMemberType) {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="w-full flex flex-col items-center justify-start gap-3 mt-4 pb-2"
-    >
-      <input
-        required
-        type="text"
-        name="id"
-        placeholder="Choose a Member ID"
-        value={newDetails.id}
-        onChange={handleChange}
-      />
-      <input
-        required
-        type="text"
-        name="name"
-        placeholder="Choose a Display Name"
-        value={newDetails.name}
-        onChange={handleChange}
-      />
-
-      <div className="flex items-center justify-between gap-2 w-full">
-        <input
-          type={show ? "text" : "password"}
-          required
-          name="password"
-          minLength={8}
-          value={newDetails.password}
-          onChange={handleChange}
-          placeholder="Create a password"
-        />
-        <SecondaryButton
-          onClick={() => setShow(!show)}
-          className="w-fit self-stretch px-2 border-0 shadow-none"
-        >
-          {show ? (
-            <Image
-              src="/icons/icon_hide.svg"
-              alt="Hide"
-              width={30}
-              height={30}
-            />
-          ) : (
-            <Image
-              src="/icons/icon_show.svg"
-              alt="Show"
-              width={30}
-              height={30}
-            />
-          )}
-        </SecondaryButton>
-      </div>
-
-      <div className="flex items-center justify-start w-full gap-2">
-        <div className="flex items-center justify-start gap-1">
-          <label htmlFor="permission" className="w-fit">
-            Set Permissions
-          </label>
-          <SecondaryButton
-            onClick={() => setShowRoles(!showRoles)}
-            className="w-fit self-stretch p-0 border-0 shadow-none"
+    <>
+      {showRoles !== "" && (
+        <Modal>
+          <ModalHeader
+            close={() => setShowRoles("")}
+            heading="View Permissions"
+          />
+          <p className="text-xs text-custom-grey-text mb-1">
+            This can be changed later.
+          </p>
+          <select
+            value={showRoles}
+            onChange={(e) => setShowRoles(e.target.value)}
           >
-            {showRoles ? (
+            {Object.keys(permissionList).map((item: any) => (
+              <option value={item} key={item}>
+                {permissionList[item].title}
+              </option>
+            ))}
+          </select>
+
+          <div className="w-full mt-2 flex items-start flex-col justify-start gap-1">
+            {Object.keys(ROLES_DESC).map((itemA: string) => {
+              const desc = ROLES_DESC[itemA];
+              const includes = permissionList[showRoles].desc.includes(
+                desc
+              ) as boolean;
+              return (
+                <div
+                  key={itemA}
+                  className="flex items-center justify-start gap-1"
+                >
+                  {includes ? (
+                    <Image
+                      alt="Yes"
+                      src="/icons/features/icon_tick.svg"
+                      width={25}
+                      height={25}
+                    />
+                  ) : (
+                    <Image
+                      alt="Yes"
+                      src="/icons/features/icon_cross_red.svg"
+                      width={25}
+                      height={25}
+                    />
+                  )}
+                  <p key={itemA} className="text-sm">
+                    {desc}
+                  </p>
+                </div>
+              );
+            })}
+          </div>
+          {permissionList[showRoles].notes && (
+            <p className="text-xs text-custom-grey-text text-start mt-1">
+              NOTE: {permissionList[showRoles].notes}
+            </p>
+          )}
+        </Modal>
+      )}
+
+      <form
+        onSubmit={handleSubmit}
+        className="w-full flex flex-col items-center justify-start gap-3 mt-4 pb-2"
+      >
+        <input
+          required
+          type="text"
+          name="id"
+          placeholder="Choose a Member ID"
+          value={newDetails.id}
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="text"
+          name="name"
+          placeholder="Choose a Display Name"
+          value={newDetails.name}
+          onChange={handleChange}
+        />
+
+        <div className="flex items-center justify-between gap-2 w-full">
+          <input
+            type={show ? "text" : "password"}
+            required
+            name="password"
+            minLength={8}
+            value={newDetails.password}
+            onChange={handleChange}
+            placeholder="Create a password"
+          />
+          <SecondaryButton
+            onClick={() => setShow(!show)}
+            className="w-fit self-stretch px-2 border-0 shadow-none"
+          >
+            {show ? (
               <Image
-                src="/icons/icon_question_primary.svg"
-                alt="Show"
+                src="/icons/icon_hide.svg"
+                alt="Hide"
                 width={30}
                 height={30}
               />
             ) : (
               <Image
-                src="/icons/icon_question.svg"
-                alt="Hide"
+                src="/icons/icon_show.svg"
+                alt="Show"
                 width={30}
                 height={30}
               />
@@ -173,55 +212,64 @@ export default function CreateNewMemberForm({ memberData }: NewMemberType) {
           </SecondaryButton>
         </div>
 
-        <select
-          name="permission"
-          id="permission"
-          className="flex-1"
-          value={newRole}
-          onChange={handleChangeSelect}
+        <div className="w-full flex flex-col items-start justify-start gap-1">
+          <div className="flex items-center justify-start gap-1">
+            <label
+              htmlFor="permission"
+              className="w-fit text-sm text-custom-dark-text"
+            >
+              Set Permissions
+            </label>
+            <SecondaryButton
+              onClick={() => setShowRoles(defaultPerms)}
+              className="w-fit self-stretch p-0 border-0 shadow-none"
+            >
+              {showRoles ? (
+                <Image
+                  src="/icons/icon_question_primary.svg"
+                  alt="Show"
+                  width={25}
+                  height={25}
+                />
+              ) : (
+                <Image
+                  src="/icons/icon_question.svg"
+                  alt="Hide"
+                  width={25}
+                  height={25}
+                />
+              )}
+            </SecondaryButton>
+          </div>
+
+          <select
+            name="permission"
+            id="permission"
+            className="flex-1"
+            value={newRole}
+            onChange={handleChangeSelect}
+          >
+            {Object.keys(permissionList).map((item: string) => {
+              return (
+                <option key={item} value={item}>
+                  {permissionList[item].title}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <PrimaryButton
+          type="submit"
+          className="grid place-items-center"
+          disabled={loading || !ready}
         >
-          {Object.keys(permissionList).map((item: string) => {
-            return (
-              <option key={item} value={item}>
-                {permissionList[item].title}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-      <PrimaryButton
-        type="submit"
-        className="grid place-items-center"
-        disabled={loading || !ready}
-      >
-        {loading ? (
-          <LoadingIconBright width={20} height={20} />
-        ) : (
-          "Create Account"
-        )}
-      </PrimaryButton>
-      {showRoles && (
-        <ul className="w-full flex flex-col items-start justify-center gap-4">
-          <p className="text-sm text-custom-grey-text">
-            This can be changed later.
-          </p>
-          {Object.keys(permissionList).map((item: any) => (
-            <li key={item} className="w-full">
-              <h1 className="text-custom-dark-text font-semibold">
-                {permissionList[item].title}
-              </h1>
-              <HRow />
-              <ul className="list-disc ml-4">
-                {permissionList[item].desc.map((itemA: string) => (
-                  <li key={itemA} className="text-sm">
-                    {itemA}
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-      )}
-    </form>
+          {loading ? (
+            <LoadingIconBright width={20} height={20} />
+          ) : (
+            "Create Account"
+          )}
+        </PrimaryButton>
+      </form>
+    </>
   );
 }
