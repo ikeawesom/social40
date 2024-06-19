1dcd9da1bf177fb56db59f51f99713bbe47241b3
ikeawesom <ikelimjl@gmail.com>
2024-06-20 00:22:18 +0800

    migrated to modals

--- src/components/profile/edit/new-member/NewMemberSection.tsx
+++ src/components/profile/edit/new-member/NewMemberSection.tsx
@@ -1,10 +1,11 @@
 "use client";
 import DefaultCard from "@/src/components/DefaultCard";
-import HRow from "@/src/components/utils/HRow";
 import { MEMBER_SCHEMA } from "@/src/utils/schemas/members";
 import React, { useState } from "react";
 import CreateNewMemberForm from "./CreateNewMemberForm";
 import Image from "next/image";
+import Modal from "@/src/components/utils/Modal";
+import ModalHeader from "@/src/components/utils/ModalHeader";
 
 export default function NewMemberSection({
   memberData,
@@ -13,34 +14,38 @@ export default function NewMemberSection({
 }) {
   const [show, setShow] = useState(false);
   return (
-    <DefaultCard className="w-full py-2 px-3">
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center justify-start gap-2">
-          <Image
-            src="/icons/navigation/icon_new_user.svg"
-            width={25}
-            height={25}
-            alt=""
-          />
-          <h1 className="text-custom-dark-text font-semibold text-start text-sm">
-            Create New Member
-          </h1>
-        </div>
-        <Image
-          src="/icons/icon_arrow-down.svg"
-          width={30}
-          height={30}
-          alt="Show"
-          onClick={() => setShow(!show)}
-          className={`duration-300 ease-in-out ${show ? "rotate-180" : ""}`}
-        />
-      </div>
+    <>
       {show && (
-        <>
-          <HRow />
+        <Modal>
+          <ModalHeader close={() => setShow(false)} heading="Create Member" />
           <CreateNewMemberForm memberData={memberData} />
-        </>
+        </Modal>
       )}
-    </DefaultCard>
+      <DefaultCard
+        onClick={() => setShow(true)}
+        className="cursor-pointer hover:brightness-95 duration-150 w-full py-2 px-3"
+      >
+        <div className="flex items-center justify-between w-full">
+          <div className="flex items-center justify-start gap-2">
+            <Image
+              src="/icons/navigation/icon_new_user.svg"
+              width={25}
+              height={25}
+              alt=""
+            />
+            <h1 className="text-custom-dark-text font-semibold text-start text-sm">
+              Create New Member
+            </h1>
+          </div>
+          <Image
+            src="/icons/icon_arrow-down.svg"
+            width={30}
+            height={30}
+            alt="Show"
+            className="-rotate-90"
+          />
+        </div>
+      </DefaultCard>
+    </>
   );
 }
