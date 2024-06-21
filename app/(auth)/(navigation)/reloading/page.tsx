7c169bbe298138cc423800cb8af41b8dc6aea542
ikeawesom <ikelimjl@gmail.com>
2024-06-21 13:37:41 +0800

    added loading title

--- app/(auth)/(navigation)/reloading/page.tsx
+++ app/(auth)/(navigation)/reloading/page.tsx
@@ -1,5 +1,10 @@
 import RefreshingScreen from "@/src/components/screens/RefreshingScreen";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Loading...",
+};
 
 export default function page() {
   return <RefreshingScreen />;
