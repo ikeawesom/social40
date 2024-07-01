72cd0a31df4ef66ee29bbd466d4cd032f6c37361
ikeawesom <ikelimjl@gmail.com>
2024-07-02 00:19:22 +0800

    sorted dates before download

--- src/components/bibo/BiboDownloadButton.tsx
+++ src/components/bibo/BiboDownloadButton.tsx
@@ -3,6 +3,7 @@ import { BIBO_DB_TYPE } from "@/src/utils/schemas/bibo";
 import SecondaryButton from "../utils/SecondaryButton";
 import Image from "next/image";
 import { ExportExcel } from "@/src/utils/helpers/ExportExcel";
+import { StringToDate } from "@/src/utils/helpers/getCurrentDate";
 
 type BiboArrType = {
   Date: string;
@@ -20,8 +21,28 @@ export default function BiboDownloadButton({
   const download = () => {
     let memberID = "";
     let biboArr = [] as BiboArrType[];
-    Object.keys(biboData).forEach((id: string) => {
-      const data = biboData[id];
+
+    const sortedBiboData = {} as BIBO_DB_TYPE;
+    const sortedBiboArr = Object.keys(biboData).sort((a, b) => {
+      const dateStrA = `${biboData[a].bookedInDate.replace(/-/gi, "/")} ${
+        biboData[a].bookedInTime
+      }`;
+      const dateStrB = `${biboData[b].bookedInDate.replace(/-/gi, "/")} ${
+        biboData[b].bookedInTime
+      }`;
+
+      return (
+        new Date(StringToDate(dateStrA).data).getTime() -
+        new Date(StringToDate(dateStrB).data).getTime()
+      );
+    }) as string[];
+
+    sortedBiboArr.forEach((id: string) => {
+      sortedBiboData[id] = biboData[id];
+    });
+
+    Object.keys(sortedBiboData).forEach((id: string) => {
+      const data = sortedBiboData[id];
       const status = data.bookedIn;
       if (memberID === "") {
         memberID = data.memberID;
