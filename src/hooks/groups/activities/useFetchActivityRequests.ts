e6ea61dcd8487768760cd71bee983e2a4ad27e1c
ikeawesom <ikelimjl@gmail.com>
2024-07-22 14:06:02 +0800

    created custom hook to fetch activity requests

---
+++ src/hooks/groups/activities/useFetchActivityRequests.ts
@@ -0,0 +1,32 @@
+import { FetchGroupActivityData } from "@/src/utils/activities/group/FetchData";
+import { useState, useEffect } from "react";
+import { useHostname } from "../../useHostname";
+import { useMemberID } from "../../useMemberID";
+import { ActivityWaitlistType } from "@/src/components/groups/custom/activities/ActivityWaitlist";
+
+export function useFetchActivityRequests(activityID: string, groupID: string) {
+  const [requested, setRequested] = useState<ActivityWaitlistType>();
+  const { memberID } = useMemberID();
+  const { host } = useHostname();
+
+  const fetchData = async () => {
+    const { error, data } = await FetchGroupActivityData.getRequests({
+      activityID,
+      groupID,
+      host,
+      memberID,
+    });
+    if (!error) {
+      const { requestsData } = data;
+      setRequested(requestsData);
+    } else {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (requested === undefined) fetchData();
+  }, [requested]);
+
+  return { requested };
+}
