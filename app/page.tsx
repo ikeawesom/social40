f06642416db2f34255b641c0b0a564ecef94cfc6
ikeawesom <ikelimjl@gmail.com>
2025-03-04 11:27:03 +0800

    added thank you screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Hero from "@/src/components/Hero";
 import PageWrapper from "@/src/components/PageWrapper";
-import PrimaryButton from "@/src/components/utils/PrimaryButton";
-import InstallButton from "@/src/components/InstallButton";
-import { VERSION_NUMBER } from "@/src/utils/versions";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-import { getMemberAuthServer } from "@/src/utils/auth/handleServerAuth";
-import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Social 40",
@@ -15,33 +9,58 @@ export const metadata: Metadata = {
 };
 
 export default async function Home() {
-  const { isAuthenticated } = await getMemberAuthServer();
-  if (isAuthenticated) redirect("/home");
-
+  const links = [
+    { name: "site", src: "globe", url: "https://ikeawesom.github.io/" },
+    { name: "git", src: "github", url: "https://github.com/ikeawesom/" },
+    {
+      name: "linkedin",
+      src: "linkedin",
+      url: "https://www.linkedin.com/in/ike-lim/",
+    },
+  ];
   return (
     <>
       <PageWrapper>
         <div className="grid place-items-center h-[80vh]">
           <div className="flex flex-col gap-y-4 items-center justify-center">
-            <Hero />
-            <div className="flex items-center justify-between gap-3 w-full max-[400px]:flex-col">
-              <InstallButton />
-              <Link scroll={false} href="/auth" className="w-full flex-1">
-                <PrimaryButton className="flex items-center justify-center text-lg">
-                  Get started
-                  <Image
-                    src="/icons/icon_right_bright.svg"
-                    alt=""
-                    width={30}
-                    height={30}
-                  />
-                </PrimaryButton>
-              </Link>
-            </div>
-            <div className="flex flex-col gap-2 items-center justify-center">
-              <p className="text-custom-grey-text text-center text-sm">
-                App Version: v{VERSION_NUMBER}
-              </p>
+            <h1 className="text-center text-4xl">
+              Goodbye{" "}
+              <span className="font-bold text-custom-primary">40 SAR</span>!
+            </h1>
+            <Image
+              alt="Thank You"
+              src="/images/thank_you.svg"
+              width={300}
+              height={300}
+            />
+
+            <h1 className="text-center text-custom-dark-text text-2xl">
+              Thank you for all the support and for using{" "}
+              <span className="font-bold">
+                Social<span className="text-custom-primary">40</span>
+              </span>
+              !
+            </h1>
+            <div>
+              Checkout my socials below
+              <div className="flex items-center justify-center gap-3 mt-2">
+                {links.map((item: any) => {
+                  return (
+                    <Link
+                      href={item.url}
+                      className="hover:opacity-75 hover:-translate-y-1 duration-150"
+                      target="_blank"
+                    >
+                      <Image
+                        alt={item.name}
+                        width={30}
+                        height={30}
+                        src={`/icons/links/${item.src}.svg`}
+                      />
+                    </Link>
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
